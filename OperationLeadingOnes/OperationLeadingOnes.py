d1b5b189269f1006f5f59feaac5e4c61c4d0e9aa
mjjeong <mjjeong@fourthlogic.co.kr>
2025-07-21 21:17:45 +0900

    Update Operation Leading Ones Example

--- OperationLeadingOnes/OperationLeadingOnes.py
+++ OperationLeadingOnes/OperationLeadingOnes.py
@@ -91,7 +91,7 @@ def main():
 			break
 		
 		# View Binary 표시 옵션 설정 # Set view binary option
-		viewImageSrc.SetPixelNumberMode(EPixelNumberMode.Binary);
+		viewImageSrc.SetPixelNumberMode(EPixelNumberMode.Hexadecimal);
 
 		# 이미지 뷰를 갱신 # Update image view
 		viewImageSrc.Invalidate(True)
