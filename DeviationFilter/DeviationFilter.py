b77d1ad768cc7878ce164db02f3c9201ca931a37
jmyu <jmyu@fourthlogic.co.kr>
2025-07-21 20:26:57 +0900

    Add Filter Examples

---
+++ DeviationFilter/DeviationFilter.py
@@ -0,0 +1,166 @@
+﻿# FLImagingClrPy 선언 // Declare FLImagingClrPy
+from FLImagingClrPy import *
+
+# 메인 함수 // Main function
+def main():
+
+	class EType:
+		Source = 0
+		Destination1 = 1
+		Destination2 = 2
+		ETypeCount = 3
+
+	# 이미지 객체 선언 // Declare the image object
+	arrFliImage = [CFLImage() for i in range(EType.ETypeCount)]
+
+	# 이미지 뷰 선언 // Declare the image view
+	arrViewImage = [CGUIViewImage() for i in range(EType.ETypeCount)]
+
+	while True:
+		
+		# Source 이미지 로드 // Load the source image
+		if (res := arrFliImage[EType.Source].Load('../../ExampleImages/EdgeDetection/Alphabat.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Destination1 이미지를 Source 이미지와 동일한 이미지로 생성 // Create destination1 image as same as source image
+		if (res := arrFliImage[EType.Destination1].Assign(arrFliImage[EType.Source])).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Destination2 이미지를 Source 이미지와 동일한 이미지로 생성 // Create destination2 image as same as source image
+		if (res := arrFliImage[EType.Destination2].Assign(arrFliImage[EType.Source])).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		bError = False
+
+		for i in range(EType.ETypeCount) :
+
+			#이미지 뷰 생성 // Create image view
+			if (res := arrViewImage[i].Create(i * 570 + 100, 0, i * 570 + 100 + 570, 427)).IsFail():
+				ErrorPrint(res, 'Failed to create the image view.')
+				bError = True
+				break
+
+			# 이미지 뷰에 이미지를 디스플레이 // Display an image in an image view
+			if (res := arrViewImage[i].SetImagePtr(arrFliImage[i])[0]).IsFail():
+				ErrorPrint(res, 'Failed to set image object on the image view.')
+				bError = True
+				break
+
+			if i == EType.Source :
+				continue
+
+			# 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the image views
+			if (res := arrViewImage[EType.Source].SynchronizePointOfView(arrViewImage[i])[0]).IsFail():
+				ErrorPrint(res, 'Failed to synchronize view.')
+				bError = True
+				break
+
+			# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
+			if (res := arrViewImage[EType.Source].SynchronizeWindow(arrViewImage[i])[0]).IsFail():
+				ErrorPrint(res, 'Failed to synchronize window.')
+				bError = True
+				break
+
+		if bError :
+			break
+
+
+		# 객체 생성 // Create object
+		DeviationFilter = CDeviationFilter()
+
+		# Source 이미지 설정 // Set the source image
+		DeviationFilter.SetSourceImage(arrFliImage[EType.Source])
+
+		# Destination 이미지 설정 // Set the destination image
+		DeviationFilter.SetDestinationImage(arrFliImage[EType.Destination1])
+
+		# 파라미터 값 설정 // Set parameter value
+		DeviationFilter.SetKernel(5)
+
+		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if (res := DeviationFilter.Execute()).IsFail():
+			ErrorPrint(res, 'Failed to execute.')
+			break
+
+		# Destination 이미지를 Destination2로 설정 // Set destination image to destination2
+		DeviationFilter.SetDestinationImage(arrFliImage[EType.Destination2])
+
+		# 파라미터 값 설정 // Set parameter value
+		DeviationFilter.SetKernel(25)
+
+		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if (res := DeviationFilter.Execute()).IsFail():
+			ErrorPrint(res, 'Failed to execute.')
+			break
+
+		
+		arrLayer = [CGUIViewImageLayer() for i in range(EType.ETypeCount)]
+
+		for i in range(EType.ETypeCount) :
+
+			# 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+			# 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+			arrLayer[i] = arrViewImage[i].GetLayer(0)
+
+			# 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+			arrLayer[i].Clear()
+
+
+		# View 정보를 디스플레이 한다. // Display view information
+		# 아래 함수 DrawTextCanvas은 Screen좌표를 기준으로 하는 String을 Drawing 한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
+		# 색상 파라미터를 EGUIViewImageLayerTransparencyColor 으로 넣어주게되면 배경색으로 처리함으로 불투명도를 0으로 한것과 같은 효과가 있다.
+		# If the color parameter is set as EGUIViewImageLayerTransparencyColor, it has the same effect as setting the opacity to 0 by treating it as a background color.
+		# 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+		#                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+		# Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+		#                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+
+
+
+		# 이미지 뷰 정보 표시 // Display image view information
+		flpPoint = CFLPoint[Double](0, 0)
+
+		if (res := arrLayer[EType.Source].DrawTextCanvas(flpPoint, 'Source Image', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		if (res := arrLayer[EType.Destination1].DrawTextCanvas(flpPoint, 'Filter Size : 5x5', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		if (res := arrLayer[EType.Destination2].DrawTextCanvas(flpPoint, 'Filter Size : 25x25', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		# 이미지 뷰를 갱신 // Update image view
+		for i in range(EType.ETypeCount) :
+			arrViewImage[i].Invalidate(True)
+
+		# 이미지 뷰가 닫히기 전까지 종료하지 않고 대기 // Wait until the image view is closed before exiting
+		bAvailable = True
+
+		while bAvailable :
+			for i in range(EType.ETypeCount) :
+				bAvailable = arrViewImage[i].IsAvailable()
+
+				if bAvailable == False :
+					break
+
+			CThreadUtilities.Sleep(1)
+
+		break
+	
+	# End of main function
+
+# 에러 출력 함수 // Error printing function
+def ErrorPrint(res, str):
+	if len(str) > 1:
+		print(str)
+
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
+
+if __name__ == '__main__':
+    main()
