b2dd7089b5f98c45ee653290223fdace14769af7
hghwang <hghwang@fourthlogic.co.kr>
2025-07-21 21:37:49 +0900

    Add Histograms of Oriented Gradients Example

---
+++ HistogramsOfOrientedGradients_PredictionImage/HistogramsOfOrientedGradients_PredictionImage.py
@@ -0,0 +1,153 @@
+﻿# FLImagingClrPy 선언 // Declare FLImagingClrPy
+from FLImagingClrPy import *
+
+
+# 메인 함수 // Main function
+def main():
+
+	# 이미지 객체 선언 // Declare the image object
+	fliSourceImage = CFLImage()
+	fliDestinationImage = CFLImage()
+
+	# 이미지 뷰 선언 // Declare the image view
+	viewImageSrc = CGUIViewImage()
+	viewImageDst = CGUIViewImage()
+
+	while True:
+		
+		# Source 이미지 로드 // Load the source image
+		if (res := fliSourceImage.Load('../../ExampleImages/OperationCompare/candle.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Destination 이미지를 Source 이미지와 동일한 이미지로 생성 // Create destination image as same as source image
+		if (res := fliDestinationImage.Assign(fliSourceImage)).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Source 이미지 뷰 생성 // Create source image view
+		if (res := viewImageSrc.Create(400, 0, 912, 484)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageSrc.SetImagePtr(fliSourceImage)[0]).IsFail():
+			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			break
+
+		# Destination 이미지 뷰 생성 // Create Destination image view
+		if (res := viewImageDst.Create(912, 0, 1424, 484)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the Destination image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageDst.SetImagePtr(fliDestinationImage)[0]).IsFail():
+			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			break
+
+		# 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views. 
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageSrc.SynchronizePointOfView(viewImageDst)[0]).IsFail() :
+			ErrorPrint(res, "Failed to synchronize view\n")
+			break
+
+		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageSrc.SynchronizeWindow(viewImageDst)[0]).IsFail() :
+			ErrorPrint(res, "Failed to synchronize window\n")
+			break
+
+		# HOG 객체 생성 // Create HOG object
+		hog = CHistogramsOfOrientedGradients()
+
+		# ROI 범위 설정 // Set the ROI value
+		flrROI = CFLRect[int](200, 10, 300, 200)
+
+		# Source 이미지 설정 // Set the source image
+		hog.SetSourceImage(fliSourceImage)
+
+		# Source ROI 설정 // Set the Source ROI
+		hog.SetSourceROI(flrROI)
+
+		# Destination 이미지 설정 // Set destination image
+		hog.SetDestinationImage(fliDestinationImage);
+
+		# Cell Size 설정 // Set Cell Size
+		hog.SetCellSize(4);
+
+		# Block Size 설정 // Set Block Size
+		hog.SetBlockSize(3);
+
+		# 비주얼 출력 타입 예측 이미지로 설정 // Set Prediction Image to visual result type
+		hog.SetVisualResultType(CHistogramsOfOrientedGradients.EVisualResultType.PredictionImage);
+
+		# 알고리즘 수행 // Execute the algorithm
+		if (res := hog.Execute()).IsFail():
+			ErrorPrint(res, 'Failed to execute Histograms Of Oriented Gradients.')
+			break
+
+		# 실행 결과를 받아오기 위한 컨테이너 // Container to get Calculated results
+		flfaPeakVectors = CFLFigureArray();
+
+		# 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		# 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		layerSource = viewImageSrc.GetLayer(0)
+		layerDestination = viewImageDst.GetLayer(0)
+
+		# 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerSource.Clear()
+		layerDestination.Clear()
+		
+		# ROI영역이 어디인지 알기 위해 디스플레이 한다 // Display to find out where ROI is
+		if (res := layerSource.DrawFigureImage(flrROI, EColor.LIME)).IsFail():
+			ErrorPrint(res, 'Failed to draw figures objects on the image view.')
+			break
+
+		if (res := layerDestination.DrawFigureImage(flrROI, EColor.LIME)).IsFail():
+			ErrorPrint(res, 'Failed to draw figures objects on the image view.')
+			break
+
+		# 이미지 뷰 정보 표시 // Display image view information
+		# 아래 함수 DrawTextCanvas은 Screen좌표를 기준으로 하는 String을 Drawing 한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
+		# 색상 파라미터를 EGUIViewImageLayerTransparencyColor 으로 넣어주게되면 배경색으로 처리함으로 불투명도를 0으로 한것과 같은 효과가 있다.
+		# If the color parameter is set as EGUIViewImageLayerTransparencyColor, it has the same effect as setting the opacity to 0 by treating it as a background color.
+		# 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+		#                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+		# Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+		#                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+		flpPoint = CFLPoint[Double](0, 0)
+
+		if (res := layerSource.DrawTextCanvas(flpPoint, 'Source Image', EColor.YELLOW, EColor.BLACK, 20)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		if (res := layerDestination.DrawTextCanvas(flpPoint, 'Destination Image', EColor.YELLOW, EColor.BLACK, 20)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		# 이미지 뷰를 갱신 합니다. // Update image view
+		viewImageSrc.Invalidate(True)
+		viewImageDst.Invalidate(True)
+
+		# 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		while viewImageSrc.IsAvailable() and viewImageDst.IsAvailable():
+			CThreadUtilities.Sleep(1)
+
+		break
+	
+	# End of main function
+
+
+
+# 에러 출력 함수 // Error printing function
+def ErrorPrint(res, str):
+	if len(str) > 1:
+		print(str)
+
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
+
+
+if __name__ == '__main__':
+    main()
