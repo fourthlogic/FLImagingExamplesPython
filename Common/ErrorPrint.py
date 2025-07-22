8fb5d92aba90bbe6e55348fc708901a29c53c090
jychoi <jychoi@fourthlogic.co.kr>
2025-07-22 09:30:42 +0900

    Add an error printing function

---
+++ Common/ErrorPrint.py
@@ -0,0 +1,11 @@
+﻿
+# FLImagingClrPy 선언 // Declare FLImagingClrPy
+from FLImagingClrPy import *
+
+# 에러 출력 함수 // Error printing function
+def ErrorPrint(res: CResult, string: str = ""):
+    if len(string) > 1:
+        print(string)
+
+    if res.IsFail():
+        print(f"Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n")
