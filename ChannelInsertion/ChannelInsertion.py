1e16830bf08841efbdb1f07dcc90d75dd1ec2747
shhwang <shhwang@fourthlogic.co.kr>
2025-07-22 10:08:09 +0900

    Modify Channel Insertion Example

--- ChannelInsertion/ChannelInsertion.py
+++ ChannelInsertion/ChannelInsertion.py
@@ -58,61 +58,61 @@ def main():
 		# 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SynchronizePointOfView(viewImageInsertion[0])[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize view.')
+			ErrorPrint(res, 'Failed to synchronize view.')
 			break
 
 		# 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageInsertion[0].SynchronizePointOfView(viewImageInsertion[1])[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize view.')
+			ErrorPrint(res, 'Failed to synchronize view.')
 			break
 
 		# 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageInsertion[1].SynchronizePointOfView(viewImageDst)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize view.')
+			ErrorPrint(res, 'Failed to synchronize view.')
 			break
 
 		# Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SetImagePtr(fliSourceImage)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
 		# Insertion 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageInsertion[0].SetImagePtr(fliInsertionImage[0])[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
 		# Insertion 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageInsertion[1].SetImagePtr(fliInsertionImage[1])[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
 		# Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageDst.SetImagePtr(fliDestinationImage)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SynchronizeWindow(viewImageInsertion[0])[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize window.')
+			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageInsertion[0].SynchronizeWindow(viewImageInsertion[1])[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize window.')
+			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageInsertion[1].SynchronizeWindow(viewImageDst)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize window.')
+			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 
 		# Channel Insertion 객체 생성 // Create Channel Insertion object
