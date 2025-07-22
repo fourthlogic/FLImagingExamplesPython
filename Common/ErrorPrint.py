d8767574d7f035e2ca3011be3fbd940b91bd36c5
jychoi <jychoi@fourthlogic.co.kr>
2025-07-22 09:47:36 +0900

    Fix Error print function

--- Common/ErrorPrint.py
+++ Common/ErrorPrint.py
@@ -3,9 +3,18 @@
 from FLImagingClrPy import *
 
 # 에러 출력 함수 // Error printing function
-def ErrorPrint(res: CResult, string: str = ""):
+def ErrorPrint(res, string: str = ""):
     if len(string) > 1:
         print(string)
 
-    if res.IsFail():
-        print(f"Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n")
+    resTmp = CResult()
+
+    if isinstance(res, tuple):
+        resTmp = res[0]
+        ref_val = res[1:]
+    else:
+        resTmp = res
+        ref_val = None
+
+    if resTmp.IsFail():
+        print(f"Error code : {resTmp.GetResultCode()}\nError name : {resTmp.GetString()}\n")
