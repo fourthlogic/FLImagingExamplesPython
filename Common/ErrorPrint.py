258cc877b253d9eb237b281f4e1ee50f0ce3ebcf
jychoi <jychoi@fourthlogic.co.kr>
2025-07-22 09:50:05 +0900

    Revert "Fix Error print function"
    This reverts commit d8767574d7f035e2ca3011be3fbd940b91bd36c5.

--- Common/ErrorPrint.py
+++ Common/ErrorPrint.py
@@ -3,18 +3,9 @@
 from FLImagingClrPy import *
 
 # 에러 출력 함수 // Error printing function
-def ErrorPrint(res, string: str = ""):
+def ErrorPrint(res: CResult, string: str = ""):
     if len(string) > 1:
         print(string)
 
-    resTmp = CResult()
-
-    if isinstance(res, tuple):
-        resTmp = res[0]
-        ref_val = res[1:]
-    else:
-        resTmp = res
-        ref_val = None
-
-    if resTmp.IsFail():
-        print(f"Error code : {resTmp.GetResultCode()}\nError name : {resTmp.GetString()}\n")
+    if res.IsFail():
+        print(f"Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n")
