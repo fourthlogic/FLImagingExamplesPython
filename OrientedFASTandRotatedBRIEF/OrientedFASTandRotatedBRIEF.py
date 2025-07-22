9a898a7d6ed0cee83d8c6e98f95ebb3e61be617e
jmyu <jmyu@fourthlogic.co.kr>
2025-07-22 10:06:32 +0900

    Fix usage of ErrorPrint function

--- OrientedFASTandRotatedBRIEF/OrientedFASTandRotatedBRIEF.py
+++ OrientedFASTandRotatedBRIEF/OrientedFASTandRotatedBRIEF.py
@@ -26,7 +26,7 @@ def main():
 		# Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SetImagePtr(fliSourceImage)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
 		# 객체 생성 // Create object
--- WhiteBalance/WhiteBalance.py
+++ WhiteBalance/WhiteBalance.py
@@ -38,32 +38,32 @@ def main():
 		# 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SynchronizePointOfView(viewImageDst)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize view.')
+			ErrorPrint(res, 'Failed to synchronize view.')
 			break
 
 		# Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SetImagePtr(fliSourceImage)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
 		# Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageDst.SetImagePtr(fliDestinationImage)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to set image object on the image view.')
+			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SynchronizeWindow(viewImageDst)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize window.')
+			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 
 		# White Balance 객체 생성 // Create White Balance object
 		whiteBalance = CWhiteBalance()
 
 		# Source 이미지 설정 // Set the source image
-		whiteBalance.SetSourceImage(fliDestinationImage)
+		whiteBalance.SetSourceImage(fliSourceImage)
 
 		# Destination 이미지 설정 // Set the destination image
 		whiteBalance.SetDestinationImage(fliDestinationImage)
