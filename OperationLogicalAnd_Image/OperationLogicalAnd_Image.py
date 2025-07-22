8f98496c48f2cb10e99926172ad61e5fdaccb30b
jmyu <jmyu@fourthlogic.co.kr>
2025-07-23 08:41:55 +0900

    Modify Operation And Example

--- OperationLogicalAnd_Image/OperationLogicalAnd_Image.py
+++ OperationLogicalAnd_Image/OperationLogicalAnd_Image.py
@@ -79,17 +79,13 @@ def main():
 		
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 # Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. # A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
-		res = viewImageSrc.SynchronizeWindow(viewImageOpr)
-
-		if res[0].IsFail():
+		if (res := viewImageSrc.SynchronizeWindow(viewImageOpr)[0]).IsFail():
 			ErrorPrint(res, 'Failed to synchronize window.')
 			break
-		
+
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 # Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. # A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
-		res = viewImageSrc.SynchronizeWindow(viewImageDst)
-
-		if res[0].IsFail():
+		if (res := viewImageSrc.SynchronizeWindow(viewImageDst)[0]).IsFail():
 			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 				
