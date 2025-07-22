9d28027f3a5d107e70654f5f667e4a75c2a299c1
jjpark <jjpark@fourthlogic.co.kr>
2025-07-22 10:25:52 +0900

    Modify Error Print in Examples

--- Contrast/Contrast.py
+++ Contrast/Contrast.py
@@ -56,7 +56,7 @@ def main():
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SynchronizeWindow(viewImageDst)[0]).IsFail():
-			ErrorPrint(res[0], 'Failed to synchronize window.')
+			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 
 		# Contrast 객체 생성 // Create Contrast object
