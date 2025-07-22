f184c41913916402dcab71174eca494677030919
sypark <sypark@fourthlogic.co.kr>
2025-07-22 18:42:36 +0900

    Add Example: GraphView

--- GraphView/GraphView.py
+++ GraphView/GraphView.py
@@ -1,111 +1,227 @@
 ﻿# FLImagingClrPy 선언 // Declare FLImagingClrPy
 from FLImagingClrPy import *
 
-import clr
-import sys
-import time
-import random
+# WinForms 관련
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import *
+from System.Drawing import Point, Size
+from System import EventHandler, Int64, Int32
 
-def main():
-    # 그래프 뷰 선언
-    arrViewGraph = [CGUIViewGraph(), CGUIViewGraph(), CGUIViewGraph()]
+class FormGraphView(Form):
+    def __init__(self):
+        Form.__init__(self)
+        self.Text = "Form Graph View"
+        self.Size = Size(420, 221)
 
-    res = CResult()
-    
-    while True:
-        if (res := arrViewGraph[0].Create(100, 0, 100 + 440, 340)).IsFail():
-            ErrorPrint(res, "Failed to create the graph view.\n")
-            break
+        self.m_bLockControls = False
+        
+        self.m_viewGraph = CGUIViewGraph()
+        
+        x1 = 10
+        x2 = 207
+        y = 10
+        cx = 187
+        cy = 23
 
-        if (res := arrViewGraph[1].Create(100 + 440 * 1, 0, 100 + 440 * 2, 340)).IsFail():
-            ErrorPrint(res, "Failed to create the graph view.\n")
-            break
+        # Buttons
+        self.buttonOpenView = Button()
+        self.buttonOpenView.Text = "Open Graph View"
+        self.buttonOpenView.Location = Point(x1, y)
+        self.buttonOpenView.Size = Size(cx, cy)
 
-        if (res := arrViewGraph[2].Create(100 + 440 * 2, 0, 100 + 440 * 3, 340)).IsFail():
-            ErrorPrint(res, "Failed to create the graph view.\n")
-            break
+        self.buttonTerminateView = Button()
+        self.buttonTerminateView.Text = "Terminate View"
+        self.buttonTerminateView.Location = Point(x2, y)
+        self.buttonTerminateView.Size = Size(cx, cy)
 
-        # 윈도우 동기화
-        if (res := arrViewGraph[0].SynchronizeWindow(arrViewGraph[1])[0]).IsFail():
-            ErrorPrint(res, "Failed to synchronize window.\n")
-            break
+        y += 30
+        
+        self.buttonLoadGraph = Button()
+        self.buttonLoadGraph.Text = "Load Graph"
+        self.buttonLoadGraph.Location = Point(x1, y)
+        self.buttonLoadGraph.Size = Size(cx, cy)
 
-        if (res := arrViewGraph[1].SynchronizeWindow(arrViewGraph[2])[0]).IsFail():
-            ErrorPrint(res, "Failed to synchronize window.\n")
-            break
+        self.buttonSaveGraph = Button()
+        self.buttonSaveGraph.Text = "Save Graph"
+        self.buttonSaveGraph.Location = Point(x2, y)
+        self.buttonSaveGraph.Size = Size(cx, cy)
+        
+        self.Controls.AddRange([self.buttonOpenView, self.buttonTerminateView, self.buttonLoadGraph, self.buttonSaveGraph])
 
-        # 차트 3개 생성
-        for k in range(3):
-            rand = random.Random(k * 2 + int(time.time() * 1000))
-            i32DataCount = 100
-            arrF64DataX = Array[Double]([0.0] * i32DataCount)
-            arrF64DataY = Array[Double]([0.0] * i32DataCount)
+        y += 35
+        
+        # GroupBox 추가
+        self.group = GroupBox()
+        self.group.Text = "Chart"
+        self.group.Location = Point(x1, y)
+        self.group.Size = Size(382, 98)
+        self.Controls.Add(self.group)
+        
+        x1 = 5
+        x2 = 197
+        y = 23
+        cx = 180
+        
+        # Label : Name
+        self.labelName = Label()
+        self.labelName.Text = "Name"
+        self.labelName.Size = Size(cx, 16)
+        self.labelName.Location = Point(x1, y)
+        self.group.Controls.Add(self.labelName)
+        
+        # Label : Type
+        self.labelType = Label()
+        self.labelType.Text = "Type"
+        self.labelType.Size = Size(cx, 16)
+        self.labelType.Location = Point(x2, y)
+        self.group.Controls.Add(self.labelType)
 
-            f64PrevX = 0
-            f64PrevY = 0
+        y+=18
 
-            for i in range(i32DataCount):
-                arrF64DataX[i] = f64PrevX + ((rand.randint(0, 99)) / 10)
-                if rand.randint(0, 1) != 0:
-                    arrF64DataY[i] = f64PrevY + ((rand.randint(0, 99)) / 10)
-                else:
-                    arrF64DataY[i] = f64PrevY - ((rand.randint(0, 99)) / 10)
+        # TextBox : Name
+        self.textboxName = TextBox()
+        self.textboxName.Location = Point(x1, y)
+        self.textboxName.Size = Size(cx, cy)
+        self.group.Controls.Add(self.textboxName)
+        
+        # ComboBox : ChartType
+        self.comboBoxChartType = ComboBox()
+        self.comboBoxChartType.DropDownStyle = ComboBoxStyle.DropDownList
+        self.comboBoxChartType.Location = Point(x2, y)
+        self.comboBoxChartType.Size = Size(cx, cy)
+        self.group.Controls.Add(self.comboBoxChartType)
 
-                f64PrevX = arrF64DataX[i]
-                f64PrevY = arrF64DataY[i]
+        self.comboBoxChartType.Items.AddRange(["Bar", "Line", "Scatter"])
+        self.comboBoxChartType.SelectedIndex = 0
+        
+        y+=26
+        
+        # Button : Add
+        self.buttonAdd = Button()
+        self.buttonAdd.Text = "Add"
+        self.buttonAdd.Location = Point(x1, y)
+        self.buttonAdd.Size = Size(cx, cy)
+        self.group.Controls.Add(self.buttonAdd)
+        
+        # Button : Clear
+        self.buttonClear = Button()
+        self.buttonClear.Text = "Clear"
+        self.buttonClear.Location = Point(x2, y)
+        self.buttonClear.Size = Size(cx, cy)
+        self.group.Controls.Add(self.buttonClear)
 
-            # EColor 무작위 RGB
-            r = rand.randint(0, 255)
-            g = rand.randint(0, 255)
-            b = rand.randint(0, 255)
-            eColor = EColor(((r | (g << 8) | (b << 16)) & 0xFFFFFFFF), True)
+        self.buttonOpenView.Click += EventHandler(self.ClickButtonOpenView)
+        self.buttonTerminateView.Click += EventHandler(self.ClickButtonTerminateView)
+        self.buttonLoadGraph.Click += EventHandler(self.ClickButtonLoadGraph)
+        self.buttonSaveGraph.Click += EventHandler(self.ClickButtonSaveGraph)
+        self.buttonAdd.Click += EventHandler(self.ClickButtonChartAdd)
+        self.buttonClear.Click += EventHandler(self.ClickButtonChartClear)
 
-            strName = f"Chart {k}"
-            
-            for view in arrViewGraph:
-                view.Plot(arrF64DataX, arrF64DataY, i32DataCount, EChartType.Line, eColor, strName)
+        # Timer
+        self.m_timer = Timer()
+        self.m_timer.Interval = 100
+        self.m_timer.Tick += EventHandler(self.TimerTick)
+        self.m_timer.Start()
 
-            time.sleep(0.005)
+        self.UpdateControls()
 
-        # 전체 차트에 Y축 최대/최소값 표시
-        eIndicateType = int(EViewGraphIndicateType.Value) | int(EViewGraphIndicateType.Name) | int(EViewGraphIndicateType.Arrow)
-        arrViewGraph[0].IndicateEntireChart(EViewGraphExtrema.MinY, EViewGraphIndicateType(eIndicateType, True))
-        arrViewGraph[0].IndicateEntireChart(EViewGraphExtrema.MaxY, EViewGraphIndicateType(eIndicateType, True))
+    def ErrorMessageBox(self, cResult, msg):
+        message = f"Error code : {cResult.GetResultCode()}\nError name : {cResult.GetString()}\n"
+        if len(msg) > 1:
+            message += msg
+        MessageBox.Show(message, "Error")
 
-        # 모든 차트 각각 X, Y 최대/최소
-        arrViewGraph[1].IndicateEveryIndividualChart(EViewGraphExtrema.MinX, EViewGraphIndicateType.All)
-        arrViewGraph[1].IndicateEveryIndividualChart(EViewGraphExtrema.MaxX, EViewGraphIndicateType.All)
-        arrViewGraph[1].IndicateEveryIndividualChart(EViewGraphExtrema.MinY, EViewGraphIndicateType.All)
-        arrViewGraph[1].IndicateEveryIndividualChart(EViewGraphExtrema.MaxY, EViewGraphIndicateType.All)
+    def LockControls(self, lock_flag):
+        self.m_bLockControls = lock_flag
+        self.UpdateControls()
 
-        # 특정 차트(인덱스 2)에 표시
-        i32ChartIndex = 2
-        indicateType = getattr(EViewGraphIndicateType, "None")
-        arrViewGraph[2].Indicate(i32ChartIndex, EViewGraphExtrema.MinX, indicateType)
-        arrViewGraph[2].Indicate(i32ChartIndex, EViewGraphExtrema.MaxX, indicateType)
-        arrViewGraph[2].Indicate(i32ChartIndex, EViewGraphExtrema.MinY, EViewGraphIndicateType.All)
-        arrViewGraph[2].Indicate(i32ChartIndex, EViewGraphExtrema.MaxY, EViewGraphIndicateType.All)
+    def TimerTick(self, sender, e):
+        self.UpdateControls()
 
-        # 십자선 비활성화
-        for view in arrViewGraph:
-            view.ShowCrosshair(False)
-            view.ZoomFit()
+    def UpdateControls(self):
+        if self.m_bLockControls:
+            enabled = False
+        elif not self.m_viewGraph.IsAvailable():
+            enabled = False
+        else:
+            enabled = True
 
-        # 뷰가 닫힐 때까지 대기
-        while all(view.IsAvailable() for view in arrViewGraph):
-            CThreadUtilities.Sleep(1)
+        self.buttonOpenView.Enabled = not enabled
+        self.buttonTerminateView.Enabled = enabled
+        self.buttonLoadGraph.Enabled = enabled
+        self.buttonSaveGraph.Enabled = self.m_viewGraph.DoesGraphExist() if enabled else False
+        self.buttonAdd.Enabled = enabled
+        self.buttonClear.Enabled = self.m_viewGraph.DoesGraphExist() if enabled else False
+        self.textboxName.Enabled = enabled
+        self.comboBoxChartType.Enabled = enabled 
 
-        break
+    def ClickButtonOpenView(self, sender, e):
+        if self.m_viewGraph.IsAvailable(): 
+            return
+        
+        if (res := self.m_viewGraph.Create(0, 0, 500, 500)).IsFail(): 
+            self.ErrorMessageBox(res, "")
 
+        self.m_viewGraph.ZoomFit()
 
+    def ClickButtonTerminateView(self, sender, e):
+        if not self.m_viewGraph.IsAvailable(): 
+            return
+        
+        if (res := self.m_viewGraph.Destroy()).IsFail(): 
+            self.ErrorMessageBox(res, "")
 
-# 에러 출력 함수 // Error printing function
-def ErrorPrint(res, str):
-	if len(str) > 1:
-		print(str)
+    def ClickButtonLoadGraph(self, sender, e):
+        if not self.m_viewGraph.IsAvailable(): 
+            return
+        self.LockControls(True)
+        self.m_viewGraph.Load("", EViewGraphLoadOption(int(EViewGraphLoadOption.Load) | int(EViewGraphLoadOption.OpenDialog), True))
+        self.LockControls(False)
 
-	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
+    def ClickButtonSaveGraph(self, sender, e):
+        if not self.m_viewGraph.IsAvailable(): 
+            return
+        if not self.m_viewGraph.DoesGraphExist(): 
+            return
+        self.LockControls(True)
+        self.m_viewGraph.Save()
+        self.LockControls(False)
 
+    def ClickButtonChartAdd(self, sender, e):
+        import random
+        if not self.m_viewGraph.IsAvailable(): 
+            return
 
-if __name__ == '__main__':
-    main()
+        # Chart Name
+        strChartName = self.textboxName.Text
+        if strChartName == "":
+            strChartName = "Chart"
+
+        # Chart Type
+        eChartType = EChartType(self.comboBoxChartType.SelectedIndex + 1, True)
+
+        # Data Count
+        i32DataCount = 30
+
+        # Data Array
+        dataX = [float(random.randint(0, 99)) for _ in range(i32DataCount)]
+        dataY = [float(random.randint(0, 99)) for _ in range(i32DataCount)]
+        arrF64DataX1 = Array[Double](dataX)
+        arrF64DataY1 = Array[Double](dataY)
+
+        # Chart Color
+        eColor = EColor((random.randint(0, 255)) | (random.randint(0, 255) << 8) | (random.randint(0, 255) << 16), True)
+
+        # Plot
+        self.m_viewGraph.Plot(arrF64DataX1, arrF64DataY1, i32DataCount, eChartType, eColor, strChartName)
+        self.m_viewGraph.Invalidate()
+
+    def ClickButtonChartClear(self, sender, e):
+        if not self.m_viewGraph.IsAvailable(): 
+            return
+        self.m_viewGraph.Clear()
+        self.m_viewGraph.Invalidate()
+
+if __name__ == "__main__":
+    Application.EnableVisualStyles()
+    Application.Run(FormGraphView())
