0459dfbec6f2797d447b921df7df2da72c4c8be8
swbang <swbang@fourthlogic.co.kr>
2025-07-23 11:15:34 +0900

    Add Cross Correlation FD Example

--- CrossCorrelationFD/CrossCorrelationFD.py
+++ CrossCorrelationFD/CrossCorrelationFD.py
@@ -6,24 +6,24 @@ from FLImagingClrPy import *
 def main():
 
 	# 이미지 객체 선언 // Declare the image object
-	fliLearnImage = CFLImage()
 	fliSourceImage = CFLImage()
+	fliOperandImage = CFLImage()
 	fliDestinationImage = CFLImage()
 
 	# 이미지 뷰 선언 // Declare the image view
-	viewImageLearn = CGUIViewImage()
 	viewImageSrc = CGUIViewImage()
+	viewImageOpr = CGUIViewImage()
 	viewImageDst = CGUIViewImage()
 
 	while True:
 		
-		# Learn 이미지 로드 // Load the learn image
-		if (res := fliLearnImage.Load('../../ExampleImages/ShadingCalibrator/Learn.flif')).IsFail():
+		# Source 이미지 로드 // Load the source image
+		if (res := fliSourceImage.Load('../../ExampleImages/CrossCorrelationFD/Cross Correlation Source.flif')).IsFail():
 			ErrorPrint(res, 'Failed to load the image file.')
 			break
 
-		# Source 이미지 로드 // Load the source image
-		if (res := fliSourceImage.Load('../../ExampleImages/ShadingCalibrator/Src.flif')).IsFail():
+		# Operand 이미지 로드 // Load the operand image
+		if (res := fliOperandImage.Load('../../ExampleImages/CrossCorrelationFD/Cross Correlation Operand.flif')).IsFail():
 			ErrorPrint(res, 'Failed to load the image file.')
 			break
 
@@ -32,13 +32,13 @@ def main():
 			ErrorPrint(res, 'Failed to load the image file.')
 			break
 
-		# Learn 이미지 뷰 생성 // Create learn image view
-		if (res := viewImageLearn.Create(100, 0, 612, 512)).IsFail():
+		# Source 이미지 뷰 생성 // Create source image view
+		if (res := viewImageSrc.Create(100, 0, 612, 512)).IsFail():
 			ErrorPrint(res, 'Failed to create the image view.')
 			break
 
-		# Source 이미지 뷰 생성 // Create the source image view
-		if (res := viewImageSrc.Create(612, 0, 1124, 512)).IsFail():
+		# Operand 이미지 뷰 생성 // Create the operand image view
+		if (res := viewImageOpr.Create(612, 0, 1124, 512)).IsFail():
 			ErrorPrint(res, 'Failed to create the image view.')
 			break
 
@@ -53,21 +53,15 @@ def main():
 			ErrorPrint(res, 'Failed to synchronize view.')
 			break
 
-		# 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
-		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
-		if (res := viewImageSrc.SynchronizePointOfView(viewImageLearn)[0]).IsFail():
-			ErrorPrint(res, 'Failed to synchronize view.')
-			break
-
-		# Learn 이미지 뷰에 이미지를 디스플레이 // Display the image in the learn image view
-		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
-		if (res := viewImageLearn.SetImagePtr(fliLearnImage)[0]).IsFail():
-			ErrorPrint(res, 'Failed to set image object on the image view.')
-			break
-
 		# Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
 		if (res := viewImageSrc.SetImagePtr(fliSourceImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# Operand 이미지 뷰에 이미지를 디스플레이 // Display the image in the operand image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageOpr.SetImagePtr(fliOperandImage)[0]).IsFail():
 			ErrorPrint(res, 'Failed to set image object on the image view.')
 			break
 
@@ -79,65 +73,63 @@ def main():
 
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
-		if (res := viewImageSrc.SynchronizeWindow(viewImageDst)[0]).IsFail():
+		if (res := viewImageSrc.SynchronizeWindow(viewImageOpr)[0]).IsFail():
 			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 
 		# 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
 		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
-		if (res := viewImageSrc.SynchronizeWindow(viewImageLearn)[0]).IsFail():
+		if (res := viewImageSrc.SynchronizeWindow(viewImageDst)[0]).IsFail():
 			ErrorPrint(res, 'Failed to synchronize window.')
 			break
 
-		# ShadingCalibrator 객체 생성 // Create ShadingCalibrator object
-		shadingCalibrator = CShadingCalibrator()
-
-		# Learn 이미지 설정 // Set the learn image
-		shadingCalibrator.SetLearnImageForShadingCalibration(fliLearnImage)
-
-		# 앞서 설정된 이미지로 Calibration을 수행 // Perform calibration with the previously set image
-		if (res := shadingCalibrator.Calibrate()).IsFail():
-			ErrorPrint(res, 'Failed to calibrate the image.')
-			break
+		# Cross Correlation FD 객체 생성 // Create Cross Correlation FD object
+		crossCorrelationFD = CCrossCorrelationFD()
 
 		# Source 이미지 설정 // Set the source image
-		shadingCalibrator.SetSourceImage(fliSourceImage)
+		crossCorrelationFD.SetSourceImage(fliSourceImage)
+
+		# Operand 이미지 설정 // Set the operand image
+		crossCorrelationFD.SetOperandImage(fliOperandImage)
 
 		# Destination 이미지 설정 // Set the destination image
-		shadingCalibrator.SetDestinationImage(fliDestinationImage)
+		crossCorrelationFD.SetDestinationImage(fliDestinationImage)
+
+		# 출력 방식 설정 // Set the output method
+		crossCorrelationFD.EnableNormalizedCrossCorrelation(True)
 
 		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
-		if (res := shadingCalibrator.Execute()).IsFail():
-			ErrorPrint(res, 'Failed to execute ShadingCalibrator.')
+		if (res := crossCorrelationFD.Execute()).IsFail():
+			ErrorPrint(res, 'Failed to execute Cross Correlation FD.')
 			break
 
 		# 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
 		# 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
-		layerLearn = viewImageLearn.GetLayer(0)
 		layerSource = viewImageSrc.GetLayer(0)
+		layerOperand = viewImageOpr.GetLayer(0)
 		layerDestination = viewImageDst.GetLayer(0)
 
 		# 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
-		layerLearn.Clear()
 		layerSource.Clear()
+		layerOperand.Clear()
 		layerDestination.Clear()
 
 		# 이미지 뷰 정보 표시 // Display image view information
 		flpPoint = CFLPoint[Double](0, 0)
 
-		if (res := layerLearn.DrawTextCanvas(flpPoint, 'Learn Image', EColor.YELLOW, EColor.BLACK, 30)).IsFail() or \
-			(res := layerSource.DrawTextCanvas(flpPoint, 'Source Image', EColor.YELLOW, EColor.BLACK, 30)).IsFail() or \
+		if (res := layerSource.DrawTextCanvas(flpPoint, 'Source Image', EColor.YELLOW, EColor.BLACK, 30)).IsFail() or \
+			(res := layerOperand.DrawTextCanvas(flpPoint, 'Operand Image', EColor.YELLOW, EColor.BLACK, 30)).IsFail() or \
 			(res := layerDestination.DrawTextCanvas(flpPoint, 'Destination Image', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
 			ErrorPrint(res, 'Failed to draw text.')
 			break
 
 		# 이미지 뷰를 갱신 // Update image view
-		viewImageLearn.Invalidate(True)
 		viewImageSrc.Invalidate(True)
+		viewImageOpr.Invalidate(True)
 		viewImageDst.Invalidate(True)
 
 		# # 이미지 뷰가 닫히기 전까지 종료하지 않고 대기 // Wait until the image view is closed before exiting
-		while viewImageLearn.IsAvailable() and viewImageSrc.IsAvailable() and viewImageDst.IsAvailable():
+		while viewImageSrc.IsAvailable() and viewImageOpr.IsAvailable() and viewImageDst.IsAvailable():
 			CThreadUtilities.Sleep(1)
 
 		break
