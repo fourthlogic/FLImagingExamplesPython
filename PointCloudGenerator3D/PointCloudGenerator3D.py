f2aba6601030a4b225add414078a22827d92bf3c
mjjeong <mjjeong@fourthlogic.co.kr>
2025-07-24 13:45:10 +0900

    Update Point Cloud Generator 3D Example

--- PointCloudGenerator3D/PointCloudGenerator3D.py
+++ PointCloudGenerator3D/PointCloudGenerator3D.py
@@ -6,12 +6,14 @@ from FLImagingClrPy import *
 def main():
 
 	# 3D 뷰 선언 // Declare the 3d view
+	view3DSrc = CGUIView3D();
 	view3DDst = CGUIView3D();
 
 	while True:
 		
 		# 3d 뷰 생성 // Create 3d object view
-		if (res := view3DDst.Create(612, 0, 1124, 512)).IsFail():
+		if (res := view3DSrc.Create(0, 0, 612, 512)).IsFail() or \
+			(res := view3DDst.Create(612, 0, 1124, 512)).IsFail():
 			ErrorPrint(res, "Failed to create 3D views")
 			break
 
@@ -19,37 +21,19 @@ def main():
 		alg = CPointCloudGenerator3D()
 		
 		# 3D 뷰와 연결이 유지된 객체 생성 // Declare the object connected to 3D view
-		view3DDst.PushObject(CFL3DObject());
-		viewObject = view3DDst.GetView3DObject(0);
-		floDst = viewObject.Get3DObject();
+		view3DDst.PushObject(CFL3DObject())
+		viewObject = view3DDst.GetView3DObject(0)
+		floDst = viewObject.Get3DObject()
 				
 		# 파라미터 설정 // Set parameter
-		alg.SetDestinationObject(floDst);
-		alg.EnableColorGeneration(True);
-		alg.EnableNormalGeneration(False);
+		alg.SetDestinationObject(floDst)
+		alg.EnableColorGeneration(True)
+		alg.EnableNormalGeneration(False)
 
-		sCount = alg.SCountInfo();
-		
-		sCount.bCopyOriginalVertices = True;
-		sCount.i64Edges = 0;
-		sCount.i64Faces = 0;
-		sCount.i64Interiors = 0;
-
-		sCount.bCopyOriginalVertices = True;
-		alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](255, 255, 255));
-		sCount.bCopyOriginalVertices = False;
-
-		sCount.i64Edges = 4000;
-		alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](255, 0, 0));
-		sCount.i64Edges = 0;
-
-		sCount.i64Faces = 20000;
-		alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](0, 255, 0));
-		sCount.i64Faces = 0;
-
-		sCount.i64Interiors = 100000;
-		alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](0, 0, 255));
-		sCount.i64Interiors = 0;
+		alg.AddPredefinedObject(alg.SCountInfo(True, 0, 0, 0), EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](255, 255, 255))
+		alg.AddPredefinedObject(alg.SCountInfo(False, 4000, 0, 0), EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](255, 0, 0))
+		alg.AddPredefinedObject(alg.SCountInfo(False, 0, 20000, 0), EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](0, 255, 0))
+		alg.AddPredefinedObject(alg.SCountInfo(False, 0, 0, 100000), EPredefinedObject.Regular_DodecaHedron, TPoint3[Byte](0, 0, 255))
 
 		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
 		if (res := alg.Execute()).IsFail():
@@ -57,9 +41,9 @@ def main():
 			break
 		
 		#출력 뷰의 시점을 계산 // Calculate the viewpoint of destination view
-		viewObject.UpdateAll();
-		view3DDst.UpdateObject(0);
-		view3DDst.ZoomFit();
+		viewObject.UpdateAll()
+		view3DDst.UpdateObject(0)
+		view3DDst.ZoomFit()
 
 		while view3DDst.IsAvailable():
 			if (res := alg.Execute()).IsFail():
@@ -69,10 +53,10 @@ def main():
 			if not view3DDst.IsAvailable():
 				break
 
-			viewObject.UpdateVertex(True);
-			view3DDst.UpdateObject(0);
+			viewObject.UpdateVertex(True)
+			view3DDst.UpdateObject(0)
 
-			view3DDst.UpdateScreen();
+			view3DDst.UpdateScreen()
 			CThreadUtilities.Sleep(1)
 
 		break
