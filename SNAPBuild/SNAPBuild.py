3c6f6b5cdb5dc180a9b926d5455cd0ba21d53901
ks <ks@fourthlogic.co.kr>
2025-07-24 19:28:47 +0900

    Add SNAP Build example

---
+++ SNAPBuild/SNAPBuild.py
@@ -0,0 +1,42 @@
+﻿# FLImagingClrPy 선언 // Declare FLImagingClrPy
+from FLImagingClrPy import *
+
+
+# 메인 함수 // Main function
+def main():
+
+	# 스냅 빌드 객체 선언 // Declare SNAP Build
+	buildSNAP = CSNAPBuild()
+
+	while True:
+
+		# 스냅 파일 로드 // Load SNAP file
+		if (res := buildSNAP.Load('C:/Users/Public/Documents/FLImaging/FLImagingExamplesSNAP/Advanced Functions/Object/Blob.flsf')).IsFail():
+			ErrorPrint(res, 'Failed to load the file.')
+			break
+
+		# 스냅 실행 // Run SNAP
+		if (res := buildSNAP.Run()).IsFail():
+			ErrorPrint(res, 'Failed to run the SNAP.')
+			break
+
+		# 스냅이 종료될 때 까지 기다림 // Wait for the SNAP to close
+		while buildSNAP.IsAvailable():
+			CThreadUtilities.Sleep(1)
+
+		break
+	
+	# End of main function
+
+
+
+# 에러 출력 함수 // Error printing function
+def ErrorPrint(res: CResult, string: str):
+	if len(string) > 1:
+		print(string)
+
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
+
+
+if __name__ == '__main__':
+    main()
