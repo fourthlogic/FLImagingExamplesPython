a319c58222f7d38cb3485525249db0fefa40921c
jychoi <jychoi@fourthlogic.co.kr>
2025-07-24 13:44:29 +0900

    Fix comment

--- BlobSubsampled_BoundaryRect/BlobSubsampled_BoundaryRect.py
+++ BlobSubsampled_BoundaryRect/BlobSubsampled_BoundaryRect.py
@@ -1,7 +1,7 @@
-﻿# FLImagingClrPy 선언 // Declare FLImagingClrPy
+﻿# FLImagingClrPy 선언 # Declare FLImagingClrPy
 from FLImagingClrPy import *
 
-# Error 출력 함수 import // Import Error Output Function
+# Error 출력 함수 import # Import Error Output Function
 import sys
 import os
 
@@ -9,39 +9,39 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "Common"))
 
 from ErrorPrint import *
 
-# 메인 함수 // Main function
+# 메인 함수 # Main function
 def main():
-    # 이미지 객체 선언 // Declare the image object
+    # 이미지 객체 선언 # Declare the image object
     fliImage = CFLImage()
 
-    # 이미지 뷰 선언 // Declare the image view
+    # 이미지 뷰 선언 # Declare the image view
     viewImage = CGUIViewImage()
     res = CResult()
 
-    # 이미지 로드 // Load image
+    # 이미지 로드 # Load image
     if (res := fliImage.Load("../../ExampleImages/Blob/Ball.flif")).IsFail():
         ErrorPrint(res, "Failed to load the image file.\n")
         return
 
-    # 이미지 뷰 생성 // Create image view
+    # 이미지 뷰 생성 # Create image view
     if (res := viewImage.Create(200, 0, 968, 576)).IsFail():
         ErrorPrint(res, "Failed to create the image view.\n")
         return
 
-    # 이미지 뷰에 이미지를 디스플레이 // Display an image in an image view
+    # 이미지 뷰에 이미지를 디스플레이 # Display an image in an image view
     if (res := viewImage.SetImagePtr(fliImage)[0]).IsFail():
         ErrorPrint(res, "Failed to set image object on the image view.\n")
         return
 
-    # Image 크기에 맞게 view의 크기를 조정 // Zoom the view to fit the image size
+    # Image 크기에 맞게 view의 크기를 조정 # Zoom the view to fit the image size
     if (res := viewImage.ZoomFit()).IsFail():
         ErrorPrint(res, "Failed to zoom fit\n")
         return
 
-    # Blob 객체 생성 // Create Blob object
+    # Blob 객체 생성 # Create Blob object
     blob = CBlobSubsampled()
 
-    # 처리할 이미지 설정 // Set the image to process
+    # 처리할 이미지 설정 # Set the image to process
     blob.SetSourceImage(fliImage)
     
 	# ROI 범위 설정
@@ -59,7 +59,7 @@ def main():
     # Subsampling Level 설정
     blob.SetSubsamplingLevel(3)
 
-    # 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+    # 앞서 설정된 파라미터 대로 알고리즘 수행 # Execute algorithm according to previously set parameters
     if (res := blob.Execute()).IsFail():
         ErrorPrint(res, "Failed to execute Blob.")
         return
@@ -82,23 +82,23 @@ def main():
         ErrorPrint(res, "Failed to get contours from the Blob object.")
         return
 
-    # 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
-    # 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+    # 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 # Obtain layer 0 number from image view for display
+    # 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 # This object belongs to an image view and does not need to be released separately
     layer = viewImage.GetLayer(0)
 
-    # 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+    # 기존에 Layer에 그려진 도형들을 삭제 # Clear the figures drawn on the existing layer
     layer.Clear()
-    # ROI영역이 어디인지 알기 위해 디스플레이 한다 // Display to find out where ROI is
+    # ROI영역이 어디인지 알기 위해 디스플레이 한다 # Display to find out where ROI is
     # FLImaging의 Figure객체들은 어떤 도형모양이든 상관없이 하나의 함수로 디스플레이가 가능				
     if (res := layer.DrawFigureImage(flrROI, EColor.BLUE)).IsFail():
         ErrorPrint(res, "Failed to draw figure objects on the image view.\n")
         return
 
     # flfaBoundaryRects 는 Figure들의 배열이기 때문에 Layer에 넣기만 해도 모두 드로윙이 가능하다.
-    # 아래 함수 DrawFigureImage는 Image좌표를 기준으로 하는 Figure를 Drawing 한다는 것을 의미하며 // The function DrawFigureImage below means drawing a picture based on the image coordinates
-    # 맨 마지막 두개의 파라미터는 불투명도 값이고 1일경우 불투명, 0일경우 완전 투명을 의미한다. // The last two parameters are opacity values, which mean opacity for 1 day and complete transparency for 0 day.
+    # 아래 함수 DrawFigureImage는 Image좌표를 기준으로 하는 Figure를 Drawing 한다는 것을 의미하며 # The function DrawFigureImage below means drawing a picture based on the image coordinates
+    # 맨 마지막 두개의 파라미터는 불투명도 값이고 1일경우 불투명, 0일경우 완전 투명을 의미한다. # The last two parameters are opacity values, which mean opacity for 1 day and complete transparency for 0 day.
     # 여기서 0.25이므로 옅은 반투명 상태라고 볼 수 있다.
-    # 파라미터 순서 : 레이어 -> Figure 객체 -> 선 색 -> 선 두께 -> 면 색 -> 펜 스타일 -> 선 알파값(불투명도) -> 면 알파값 (불투명도) // Parameter order: Layer -> Figure object -> Line color -> Line thickness -> Face color -> Pen style -> Line alpha value (opacity) -> Area alpha value (opacity)
+    # 파라미터 순서 : 레이어 -> Figure 객체 -> 선 색 -> 선 두께 -> 면 색 -> 펜 스타일 -> 선 알파값(불투명도) -> 면 알파값 (불투명도) # Parameter order: Layer -> Figure object -> Line color -> Line thickness -> Face color -> Pen style -> Line alpha value (opacity) -> Area alpha value (opacity)
     if (res := layer.DrawFigureImage(flfaBoundaryRects, EColor.RED, 1, EColor.RED, EGUIViewImagePenStyle.Solid, 1.0, .25)).IsFail():
         ErrorPrint(res, "Failed to draw figure objects on the image view.\n")
         return
@@ -121,7 +121,7 @@ def main():
         layer.DrawTextImage(flrRect.GetCenter(), strIndex, EColor.CYAN);
 
 
-    # 이미지 뷰를 갱신 합니다. // Update image view
+    # 이미지 뷰를 갱신 합니다. # Update image view
     viewImage.Invalidate()
 
     while viewImage.IsAvailable():
--- CircleGauge/CircleGauge.py
+++ CircleGauge/CircleGauge.py
@@ -1,7 +1,7 @@
-﻿# FLImagingClrPy 선언 // Declare FLImagingClrPy
+﻿# FLImagingClrPy 선언 # Declare FLImagingClrPy
 from FLImagingClrPy import *
 
-# Error 출력 함수 import // Import Error Output Function
+# Error 출력 함수 import # Import Error Output Function
 import sys
 import os
 
@@ -10,7 +10,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "Common"))
 from ErrorPrint import *
 
 
-# 메인 함수 // Main function
+# 메인 함수 # Main function
 def main():
     arrViewText = [
         "Dark To Bright Or Bright To Dark\nBegin 0",
@@ -59,19 +59,19 @@ def main():
 
     i32ExampleCount = len(arrViewText)
 
-    # 이미지 객체 선언 // Declare the image object
+    # 이미지 객체 선언 # Declare the image object
     fliImage = CFLImage()
 
-    # 이미지 뷰 선언 // Declare the image view
+    # 이미지 뷰 선언 # Declare the image view
     arrViewImage = [CGUIViewImage() for v in range(i32ExampleCount)]
     res = CResult()
 
-    # 이미지 로드 // Load image
+    # 이미지 로드 # Load image
     if (res := fliImage.Load("../../ExampleImages/Gauge/Circle.flif")).IsFail():
         ErrorPrint(res, "Failed to load the image file.\n")
         return
 
-    # 이미지 뷰 생성 // Create image view
+    # 이미지 뷰 생성 # Create image view
     for i in range(i32ExampleCount):        
         i32X = 300 * (i % 4)
         i32Y = 300 * int(i / 4)
@@ -80,63 +80,63 @@ def main():
             ErrorPrint(res, "Failed to create the image view.\n")
             break
 
-        # 이미지 뷰에 이미지를 디스플레이 // display the image in the imageview
+        # 이미지 뷰에 이미지를 디스플레이 # display the image in the imageview
         if (res := arrViewImage[i].SetImagePtr(fliImage)[0]).IsFail():
             ErrorPrint(res, "Failed to set image object on the image view.\n")
             break
 
-        # 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the all image views. 
+        # 이미지 뷰의 시점을 동기화 한다 # Synchronize the viewpoints of the all image views. 
         if i > 0:
             if (res := arrViewImage[i].SynchronizePointOfView(arrViewImage[0])[0]).IsFail():
                 ErrorPrint(res, "Failed to set image object on the image view.\n")
                 break
 
-    # Circle Gauge 객체 생성 // Create Circle Gauge Object
+    # Circle Gauge 객체 생성 # Create Circle Gauge Object
     circleGauge = CCircleGauge()
     
-    # 처리할 이미지 설정 // Set the image to process
+    # 처리할 이미지 설정 # Set the image to process
     circleGauge.SetSourceImage(fliImage)
 
-    # 측정할 영역을 설정합니다. // Set the area to measure.
+    # 측정할 영역을 설정합니다. # Set the area to measure.
     measureRegion = CFLCircle[Double](260.0, 265.0, 120.0)
     tolerance = 100
     circleGauge.SetMeasurementRegion(measureRegion, tolerance)
 
-    # 추출하기위한 파라미터를 설정합니다. // Set parameters for extraction.                
-    # 원을 추정하기위해 추출할 경계점의 변화 임계값에 대해 설정합니다. // Set the threshold change of the boundary point to be extracted to estimate the circle.
+    # 추출하기위한 파라미터를 설정합니다. # Set parameters for extraction.                
+    # 원을 추정하기위해 추출할 경계점의 변화 임계값에 대해 설정합니다. # Set the threshold change of the boundary point to be extracted to estimate the circle.
     circleGauge.SetThreshold(20)
-    # 원을 추정하기위해 추출할 경계점의 변화 임계값에 보정값을 설정합니다. // Set the correction value to the threshold change of the boundary point to be extracted to estimate the circle.
+    # 원을 추정하기위해 추출할 경계점의 변화 임계값에 보정값을 설정합니다. # Set the correction value to the threshold change of the boundary point to be extracted to estimate the circle.
     circleGauge.SetMinimumAmplitude(10)
-    # 원을 추정하기위해 추출할 경계점들의 대표값 표본 개수를 설정합니다. // Set the number of representative sample values ??of the boundary points to be extracted to estimate the circle.
+    # 원을 추정하기위해 추출할 경계점들의 대표값 표본 개수를 설정합니다. # Set the number of representative sample values ??of the boundary points to be extracted to estimate the circle.
     circleGauge.SetThickness(3)
-    # 원을 추정하기위해 추출할 경계점들의 추출 간격을 설정합니다. // Set the extraction interval of boundary points to be extracted to estimate the circle.
+    # 원을 추정하기위해 추출할 경계점들의 추출 간격을 설정합니다. # Set the extraction interval of boundary points to be extracted to estimate the circle.
     circleGauge.SetSamplingStep(1)
-    # 원을 추정하기위해 추출할 경계점들의 이상치 조정을 위한 임계값을 설정합니다. // Set the threshold value for outlier adjustment of the boundary points to be extracted to estimate the circle.
+    # 원을 추정하기위해 추출할 경계점들의 이상치 조정을 위한 임계값을 설정합니다. # Set the threshold value for outlier adjustment of the boundary points to be extracted to estimate the circle.
     circleGauge.SetOutliersThreshold(3)
-    # 원을 추정하기위해 추출할 경계점들의 이상치 조정 횟수을 설정합니다. // Set the number of outlier adjustments for boundary points to be extracted to estimate the circle.
+    # 원을 추정하기위해 추출할 경계점들의 이상치 조정 횟수을 설정합니다. # Set the number of outlier adjustments for boundary points to be extracted to estimate the circle.
     circleGauge.SetOutliersThresholdCount(3)
 
     for i in range(i32ExampleCount):
-        # 원을 추정하기위해 추출할 경계점 변화 방향에 대해 설정합니다. // Set the boundary point change direction to extract to estimate the circle.
+        # 원을 추정하기위해 추출할 경계점 변화 방향에 대해 설정합니다. # Set the boundary point change direction to extract to estimate the circle.
         circleGauge.SetTransitionType(arrTransitionType[i])
-        # 원을 추정하기위해 추출한 경계점 중 사용할 경계점 유형을 선택합니다. // Select the boundary point type to use among the boundary points extracted to estimate the circle.
+        # 원을 추정하기위해 추출한 경계점 중 사용할 경계점 유형을 선택합니다. # Select the boundary point type to use among the boundary points extracted to estimate the circle.
         circleGauge.SetTransitionChoice(arrTransitionChoice[i])
 
-        # 알고리즘 수행 // Execute the Algoritm
+        # 알고리즘 수행 # Execute the Algoritm
         if (res := circleGauge.Execute()).IsFail():
             ErrorPrint(res, "Failed to execute Circle gauge.")
             break
 
-        # 실행 결과를 가져옵니다. // Get the execution result.
+        # 실행 결과를 가져옵니다. # Get the execution result.
         flcResult = CFLCircle[Double]()
         flfaResultsValid = CFLFigureArray()
         flfaResultsInvalid = CFLFigureArray()
 
-        # 실행 결과를 가져옵니다. // Get the execution result. // Get the execution result.
+        # 실행 결과를 가져옵니다. # Get the execution result. # Get the execution result.
         res = circleGauge.GetMeasuredObject(flcResult, i % 4)[0]
-        # 추정된 원을 추출에 사용된 유효 경계점을 가져옵니다. // Get the effective boundary point used to extract the estimated circle.
+        # 추정된 원을 추출에 사용된 유효 경계점을 가져옵니다. # Get the effective boundary point used to extract the estimated circle.
         circleGauge.GetMeasuredValidPoints(flfaResultsValid, i % 4)
-        # 추정된 원을 추출에 사용되지 못한 유효하지 않은 경계점을 가져옵니다. // Get an invalid boundary point that is not used to extract the estimated circle.
+        # 추정된 원을 추출에 사용되지 못한 유효하지 않은 경계점을 가져옵니다. # Get an invalid boundary point that is not used to extract the estimated circle.
         circleGauge.GetMeasuredInvalidPoints(flfaResultsInvalid, i % 4)
 
         layer = arrViewImage[i].GetLayer(0)
@@ -147,14 +147,14 @@ def main():
             ErrorPrint(res, "Failed to draw figure\n")
             break
 
-        # 측정 영역이 어디인지 알기 위해 디스플레이 한다 // Display to know where the measurement area is
+        # 측정 영역이 어디인지 알기 위해 디스플레이 한다 # Display to know where the measurement area is
         flcRegion = circleGauge.GetMeasurementRegion()
 
         flcInner = CFLCircle[Double]()
         flcOuter = CFLCircle[Double]()
         f64Tolerance = circleGauge.GetTolerance()
 
-        # 설정된 ROI에 대해 내부 및 외부 측정영역을 디스플레이 합니다. // Display the inner and outer measurement areas for the set ROI.
+        # 설정된 ROI에 대해 내부 및 외부 측정영역을 디스플레이 합니다. # Display the inner and outer measurement areas for the set ROI.
 
         flpRegionCenter = flcRegion.GetCenter()
 
@@ -174,7 +174,7 @@ def main():
             break
 
         if res.IsOK():
-            # 추정된 원을 디스플레이 합니다. // Display the estimated circle.
+            # 추정된 원을 디스플레이 합니다. # Display the estimated circle.
             if (res := layer.DrawFigureImage(flcResult, EColor.BLACK, 5)).IsFail():
                 ErrorPrint(res, "Failed to draw figure")
                 break
@@ -183,14 +183,14 @@ def main():
                 ErrorPrint(res, "Failed to draw figure")
                 break
 
-            # 원의 정보를 Console창에 출력합니다. // Output the original information to the console window.
+            # 원의 정보를 Console창에 출력합니다. # Output the original information to the console window.
             f64Radius = 0
             flcResult.GetRadius(f64Radius)
             flpLineCenter = CFLPoint[Double]()
             flcResult.GetCenter(flpLineCenter)
             Console.WriteLine("Circle Center : ({}, {})\nRadius : {} pixels".format(flpLineCenter.x, flpLineCenter.y, f64Radius))
 
-        # 추출된 유효점이 어디인지 알기 위해 디스플레이 한다 // Display to know where the extracted valid point is
+        # 추출된 유효점이 어디인지 알기 위해 디스플레이 한다 # Display to know where the extracted valid point is
         for i64Index in range(flfaResultsValid.GetCount()):
             if flfaResultsValid.GetAt(i64Index).GetDeclType() != EFigureDeclType.Point:
                 break
@@ -206,7 +206,7 @@ def main():
                 ErrorPrint(res, "Failed to draw figure")
                 break
 
-        # 추출된 유효하지 않은 점이 어디인지 알기 위해 디스플레이 한다 // Display to see where the extracted invalid points are
+        # 추출된 유효하지 않은 점이 어디인지 알기 위해 디스플레이 한다 # Display to see where the extracted invalid points are
         for i64Index in range(flfaResultsInvalid.GetCount()):
             if flfaResultsInvalid.GetAt(i64Index).GetDeclType() != EFigureDeclType.Point:
                 break
@@ -222,12 +222,12 @@ def main():
                 ErrorPrint(res, "Failed to draw figure")
                 break
 
-        # 측정 영역이 어디인지 알기 위해 디스플레이 한다 // Display to know where the measurement area is
+        # 측정 영역이 어디인지 알기 위해 디스플레이 한다 # Display to know where the measurement area is
         if (res := layer.DrawFigureImage(measureRegion, EColor.BLUE)).IsFail():
             ErrorPrint(res, "Failed to draw figures objects on the image view.")
             break
 
-        # 이미지 뷰를 갱신 합니다. // Update the image view.
+        # 이미지 뷰를 갱신 합니다. # Update the image view.
         arrViewImage[i].Invalidate()
         
     bAvailable = True
