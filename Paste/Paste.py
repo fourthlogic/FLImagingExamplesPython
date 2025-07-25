09d9bc390d828d755409a8f32837c19874763e18
osa <osa@fourthlogic.co.kr>
2025-07-25 09:06:52 +0900

    Add Paste Example

---
+++ Paste/Paste.py
@@ -0,0 +1,126 @@
+﻿# FLImagingClrPy 선언 // Declare FLImagingClrPy
+from FLImagingClrPy import *
+
+
+# 메인 함수 // Main function
+def main():
+
+	# 이미지 객체 선언 // Declare the image object
+	fliSourceImage = CFLImage()
+	fliDestinationImage = CFLImage()
+
+	# 이미지 뷰 선언 // Declare the image view
+	viewImageSrc = CGUIViewImage()
+	viewImageDst = CGUIViewImage()
+
+	while True:
+		
+		# Source 이미지 로드 // Load the source image
+		if (res := fliSourceImage.Load('../../ExampleImages/Paste/Moon.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Source 이미지 뷰 생성 // Create source image view
+		if (res := viewImageSrc.Create(400, 0, 912, 612)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Destination 이미지 뷰 생성 // Create the destination image view
+		if (res := viewImageDst.Create(912, 0, 1424, 612)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageSrc.SetImagePtr(fliSourceImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. // A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageDst.SetImagePtr(fliDestinationImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# Image 크기에 맞게 view의 크기를 조정 # Zoom the view to fit the image size
+		if (res := viewImageSrc.ZoomFit()).IsFail():
+		    ErrorPrint(res, "Failed to zoom fit\n")
+		    return
+
+		# Color Converter 객체 생성 // Create Color Converter object
+		paste = CPaste()
+
+		# Source 이미지 설정 // Set the source image
+		paste.SetSourceImage(fliSourceImage)
+
+		# ROI 설정 하기 위해 FLCircle 생성 // Create a FLCircle for ROI setting
+		flcROI = CFLCircle[Double](256, 169, 25)
+
+		# 처리할 ROI 설정 // Set the ROI to be processed 
+		paste.SetSourceROI(flcROI)
+
+		# Destination 이미지 설정 // Set the destination image
+		paste.SetDestinationImage(fliDestinationImage)
+
+		# 항상 공백 영역을 지정한 색으로 채우도록 설정 // Set the blank area to always be filled with a specified color 
+		paste.EnableFillBlankColorMode(True)
+
+		# 공백 영역 색상 지정 // Set the blank color
+		mvBlankColor = CMultiVar[Double](255, 255, 255)
+		paste.SetBlankColor(mvBlankColor)
+
+		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if (res := paste.Execute()).IsFail():
+			ErrorPrint(res, 'Failed to execute Color Converter.')
+			break
+
+		# 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+		# 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		layerSource = viewImageSrc.GetLayer(0)
+		layerDestination = viewImageDst.GetLayer(0)
+
+		# 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		layerSource.Clear()
+		layerDestination.Clear()
+
+		# ROI영역이 어디인지 알기 위해 디스플레이 한다 // Display to find out where ROI is
+		# FLImaging의 Figure 객체들은 어떤 도형모양이든 상관없이 하나의 함수로 디스플레이가 가능 // FLimaging's Figure objects can be displayed as a function regardless of the shape
+		# 아래 함수 DrawFigureImage는 Image좌표를 기준으로 하는 Figure를 Drawing 한다는 것을 의미하며 // The function DrawFigureImage below means drawing a picture based on the image coordinates
+		# 맨 마지막 두개의 파라미터는 불투명도 값이고 1일경우 불투명, 0일경우 완전 투명을 의미한다. // The last two parameters are opacity values, which mean opacity for 1 day and complete transparency for 0 day.
+		# 파라미터 순서 : 레이어 -> Figure 객체 -> 선 색 -> 선 두께 -> 면 색 -> 펜 스타일 -> 선 알파값(불투명도) -> 면 알파값 (불투명도) // Parameter order: Layer -> Figure object -> Line color -> Line thickness -> Face color -> Pen style -> Line alpha value (opacity) -> Area alpha value (opacity)
+		if (res := layerSource.DrawFigureImage(flcROI, EColor.BLUE)).IsFail():
+			ErrorPrint(res, 'Failed to draw figures objects on the image view.')
+			break
+
+		# 이미지 뷰 정보 표시 // Display image view information
+		flpPoint = CFLPoint[Double](0, 0)
+		
+		if (res := layerSource.DrawTextCanvas(flpPoint, 'Source Image', EColor.YELLOW, EColor.BLACK, 20)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		if (res := layerDestination.DrawTextCanvas(flpPoint, 'Destination Image', EColor.YELLOW, EColor.BLACK, 20)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		# 이미지 뷰를 갱신 // Update image view
+		viewImageSrc.Invalidate(True)
+		viewImageDst.Invalidate(True)
+
+		# # 이미지 뷰가 닫히기 전까지 종료하지 않고 대기 // Wait until the image view is closed before exiting
+		while viewImageSrc.IsAvailable() and viewImageDst.IsAvailable():
+			CThreadUtilities.Sleep(1)
+
+		break
+	
+	# End of main function
+
+# 에러 출력 함수 // Error printing function
+def ErrorPrint(res: CResult, string: str):
+	if len(string) > 1:
+		print(string)
+
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
+
+if __name__ == '__main__':
+    main()
