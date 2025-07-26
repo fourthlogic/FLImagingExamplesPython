869fd47bfdf39c84856586dd56c4fc30f0355536
jhsong <jhsong@fourthlogic.co.kr>
2025-07-26 16:54:40 +0900

    Revert "Modify Examples"
    This reverts commit ebb47c8df6e61f055558e3a90567d84b1fc30b82.

--- ColorizedPointCloudGenerator3D/ColorizedPointCloudGenerator3D.py
+++ ColorizedPointCloudGenerator3D/ColorizedPointCloudGenerator3D.py
@@ -1,8 +1,6 @@
 ﻿# FLImagingClrPy 선언 # Declare FLImagingClrPy
 from FLImagingClrPy import *
 
-CLibraryUtilities.Initialize()
-
 # Error 출력 함수 import // Import Error Output Function
 import sys
 import os
