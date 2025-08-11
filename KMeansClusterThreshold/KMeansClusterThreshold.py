fc73364ccc7d945f1343da9daf0b681e0a3ddae0
jjpark <jjpark@fourthlogic.co.kr>
2025-08-11 09:12:00 +0900

    Rename class instance of K-means Cluster Threshold

--- KMeansClusterThreshold/KMeansClusterThreshold.py
+++ KMeansClusterThreshold/KMeansClusterThreshold.py
@@ -80,41 +80,41 @@ def main():
 
 
 		# K-means Cluster Threshold 객체 생성 // Create K-means Cluster Threshold object
-		kMeans = CKMeansClusterThreshold()
+		kMeansClusterThreshold = CKMeansClusterThreshold()
 
 		# Source 이미지 설정 // Set the source image
-		kMeans.SetSourceImage(arrFliImage[EType.Source])
+		kMeansClusterThreshold.SetSourceImage(arrFliImage[EType.Source])
 
 		# Destination 이미지 설정 // Set the destination image
-		kMeans.SetDestinationImage(arrFliImage[EType.Destination1])
+		kMeansClusterThreshold.SetDestinationImage(arrFliImage[EType.Destination1])
 
 		# Cluster 갯수 설정 // Set the number of clusters
-		kMeans.SetNumOfCluster(2)
+		kMeansClusterThreshold.SetNumOfCluster(2)
 
 		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
-		if (res := kMeans.Execute()).IsFail():
+		if (res := kMeansClusterThreshold.Execute()).IsFail():
 			ErrorPrint(res, 'Failed to execute K-means Cluster Threshold.')
 			break
 
 		# Destination 이미지를 Destination2로 설정 // Set destination image to destination2
-		kMeans.SetDestinationImage(arrFliImage[EType.Destination2])
+		kMeansClusterThreshold.SetDestinationImage(arrFliImage[EType.Destination2])
 
 		# Cluster 갯수 설정 // Set the number of clusters
-		kMeans.SetNumOfCluster(4)
+		kMeansClusterThreshold.SetNumOfCluster(4)
 
 		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
-		if (res := kMeans.Execute()).IsFail():
+		if (res := kMeansClusterThreshold.Execute()).IsFail():
 			ErrorPrint(res, 'Failed to execute K-means Cluster Threshold.')
 			break
 
 		# Destination 이미지를 Destination3로 설정 // Set destination image to destination3
-		kMeans.SetDestinationImage(arrFliImage[EType.Destination3])
+		kMeansClusterThreshold.SetDestinationImage(arrFliImage[EType.Destination3])
 
 		# Cluster 갯수 설정 // Set the number of clusters
-		kMeans.SetNumOfCluster(8)
+		kMeansClusterThreshold.SetNumOfCluster(8)
 
 		# 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
-		if (res := kMeans.Execute()).IsFail():
+		if (res := kMeansClusterThreshold.Execute()).IsFail():
 			ErrorPrint(res, 'Failed to execute K-means Cluster Threshold.')
 			break
 		
