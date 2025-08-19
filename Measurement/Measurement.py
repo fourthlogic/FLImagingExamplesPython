a97f54614e8dccf2fec2dbc9404bd6ede08b9f85
hskim <hskim@fourthlogic.co.kr>
2025-08-19 11:56:01 +0900

    Fix Error Print of Measurement Example

--- Measurement/Measurement.py
+++ Measurement/Measurement.py
@@ -177,7 +177,7 @@ def ErrorPrint(res, str):
 	if len(str) > 1:
 		print(str)
 
-	print(f'Error code : res.GetResultCode()\nError name : res.GetString()')
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
 
 def Calibration(orthogonalCalibrator, fliBoardImage):
 	bResult = False
