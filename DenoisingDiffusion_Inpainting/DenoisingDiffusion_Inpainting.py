12dabcac3135c9fcdf22d8bb8240bdb402d91898
sbno <sbno@fourthlogic.co.kr>
2025-10-22 14:41:47 +0900

    Add Denoising Diffusion Inpainting Example

---
+++ DenoisingDiffusion_Inpainting/DenoisingDiffusion_Inpainting.py
@@ -0,0 +1,333 @@
+﻿# FLImagingClrPy 선언 # Declare FLImagingClrPy
+from tokenize import Single
+from FLImagingClrPy import *
+
+# You must call the following function once
+# before using any features of the FLImaging(R) library
+CLibraryUtilities.Initialize()
+
+import threading
+import time
+import msvcrt
+
+bEscape = False
+bTerminated = False
+eLearnResult = CResult()
+
+# 메인 함수 # Main function
+def main():
+
+	# 이미지 객체 선언 # Declare the image object
+	fliLearnImage = CFLImage()
+	fliValidateImage = CFLImage()
+	fliSourceImage = CFLImage()
+	fliResultImage = CFLImage()
+
+	# 이미지 뷰 선언 # Declare the image view
+	viewImageLearn = CGUIViewImage()
+	viewImageValidate = CGUIViewImage()
+	viewImageSource = CGUIViewImage()
+	viewImageResult = CGUIViewImage()
+
+	# 그래프 뷰 선언 # Declare the graph view
+	viewGraph = CGUIViewGraph()
+	bTerminated = False
+
+	while True:
+		# 라이브러리가 완전히 로드 될 때까지 기다림 # Wait for the library to fully load
+		time.sleep(1)
+
+		# Learn 이미지 로드 # Load the learn image
+		if (res := fliLearnImage.Load('../../ExampleImages/DenoisingDiffusion/Ball_Train.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Validation 이미지 로드 # Load the validation image
+		if (res := fliValidateImage.Load('../../ExampleImages/DenoisingDiffusion/Ball_Validation.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Source 이미지 로드 # Load the Source image
+		if (res := fliSourceImage.Load('../../ExampleImages/DenoisingDiffusion/Ball_Source.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# Learn 이미지 뷰 생성 # Create learn image view
+		if (res := viewImageLearn.Create(100, 0, 612, 512)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Validation 이미지 뷰 생성 # Create the validation image view
+		if (res := viewImageValidate.Create(612, 0, 1124, 512)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Result 이미지 뷰 생성 # Create source image view
+		if (res := viewImageSource.Create(100, 512, 612, 1024)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Result 이미지 뷰 생성 # Create source image view
+		if (res := viewImageResult.Create(612, 512, 1124, 1024)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+
+		# Graph 뷰 생성 # Create graph view
+		if (res := viewGraph.Create(1124, 512, 1636, 1024)).IsFail():
+			ErrorPrint(res, 'Failed to create the graph view.')
+			break
+
+		# Learn 이미지 뷰에 이미지를 디스플레이 # Display the image in the learn image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. # A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageLearn.SetImagePtr(fliLearnImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# Validation 이미지 뷰에 이미지를 디스플레이 # Display the image in the validation image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. # A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageValidate.SetImagePtr(fliValidateImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# Result 이미지 뷰에 이미지를 디스플레이 # Display the image in the source image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. # A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageSource.SetImagePtr(fliSourceImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# Result 이미지 뷰에 이미지를 디스플레이 # Display the image in the source image view
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. # A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageResult.SetImagePtr(fliResultImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# 이미지 뷰의 시점을 동기화 한다 # Synchronize the viewpoints of the image views
+		# ref 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [ref 0], ... [ref n-1] 형태로 tuple 을 반환한다. # A function that receives ref parameters returns a tuple structured as [return], [ref 0], ... [ref n-1].
+		if (res := viewImageLearn.SynchronizePointOfView(viewImageValidate)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize view.')
+			break
+
+		if (res := viewImageLearn.SynchronizeWindow(viewImageValidate)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize window.')
+			break
+
+		# 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 # Obtain layer 0 number from image view for display
+		# 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 # This object belongs to an image view and does not need to be released separately
+		layerLearn = viewImageLearn.GetLayer(0)
+		layerValidate = viewImageValidate.GetLayer(0)
+		layerSource = viewImageSource.GetLayer(0)
+		layerResult = viewImageResult.GetLayer(0)
+
+		# 기존에 Layer에 그려진 도형들을 삭제 # Clear the figures drawn on the existing layer
+		layerLearn.Clear()
+		layerValidate.Clear()
+		layerSource.Clear()
+		layerResult.Clear()
+
+		# 이미지 뷰 정보 표시 # Display image view information
+		flpPoint = CFLPoint[Double](0, 0)
+
+		if (res := layerLearn.DrawTextCanvas(flpPoint, 'LEARN', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		if (res := layerValidate.DrawTextCanvas(flpPoint, 'VALIDATE', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		if (res := layerSource.DrawTextCanvas(flpPoint, 'Source', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		if (res := layerResult.DrawTextCanvas(flpPoint, 'Result', EColor.YELLOW, EColor.BLACK, 30)).IsFail():
+			ErrorPrint(res, 'Failed to draw text.')
+			break
+
+		# 객체 생성 # Create object
+		denoisingDiffusionInpaintingDL = CDenoisingDiffusionInpaintingDL()
+
+		# 학습할 이미지 설정 # Set the image to learn
+		denoisingDiffusionInpaintingDL.SetLearningImage(fliLearnImage)
+		# 검증할 이미지 설정 # Set the image to validate
+		denoisingDiffusionInpaintingDL.SetLearningValidationImage(fliValidateImage)
+		
+		# 학습할 DenoisingDiffusionInpaintingDL 모델 설정 # Set up the DenoisingDiffusionInpaintingDL model to learn
+		denoisingDiffusionInpaintingDL.SetModel(CDenoisingDiffusionInpaintingDL.EModel.FLGenNet_Diffusion_Inpainting)
+		# 학습할 DenoisingDiffusionInpaintingDL 모델 버전 설정 # Set up the DenoisingDiffusionInpaintingDL model version to learn
+		denoisingDiffusionInpaintingDL.SetModelVersion(CDenoisingDiffusionInpaintingDL.EModelVersion.FLGenNet_Diffusion_Inpainting_V1_256)
+		# 학습 epoch 값을 설정 # Set the learn epoch value 
+		denoisingDiffusionInpaintingDL.SetLearningEpoch(2500)
+		# 학습 이미지 Interpolation 방식 설정 # Set Interpolation method of learn image
+		denoisingDiffusionInpaintingDL.SetInterpolationMethod(EInterpolationMethod.Bilinear)
+		# 모델의 최적의 상태를 추적 후 마지막에 최적의 상태로 적용할 지 여부 설정 # Set whether to track the optimal state of the model and apply it as the optimal state at the end.
+		denoisingDiffusionInpaintingDL.EnableOptimalLearningStatePreservation(True)
+
+		# OptimizerSpec 객체 생성 # Create OptimizerSpec object
+		optSpec = COptimizerSpecAdamGradientDescent()
+		# Optimizer의 학습률 설정 # Set learning rate of Optimizer
+		optSpec.SetLearningRate(0.0001)
+		# Optimizer의 Weight Decay 설정 # Set weight decay of Optimizer
+		optSpec.SetWeightDecay(.0)
+		# 설정한 Optimizer를 DenoisingDiffusionInpaintingDL에 적용 # Apply Optimizer that we set up to DenoisingDiffusionInpaintingDL
+		denoisingDiffusionInpaintingDL.SetLearningOptimizerSpec(optSpec)
+
+		# 자동 저장 옵션 설정 # Set Auto-Save Options
+		autoSaveSpec = CAutoSaveSpec()
+
+		# 자동 저장 활성화 # Enable Auto-Save
+		# 저장 때문에 발생하는 속도 저하를 막기 위해 예제에서는 코드 사용법만 표시하고 옵션은 끔 # To prevent performance degradation caused by saving, the examples only demonstrate how to use the code, with the saving option disabled.
+		autoSaveSpec.EnableAutoSave(False)
+		# 저장할 모델 경로 설정 # Set Model path to save
+		autoSaveSpec.SetAutoSavePath('model.flgani')
+		# 자동 저장 조건식 설정. 현재 f1score값이 최대 값인 경우 저장 활성화
+		# Set auto-save conditional expressions. Enable save if the current f1score value is the maximum value
+		autoSaveSpec.SetAutoSaveCondition("epoch >= 10 & metric > max('metric')")
+
+		# 자동 저장 옵션 설정 # Set Auto-Save Options
+		denoisingDiffusionInpaintingDL.SetLearningAutoSaveSpec(autoSaveSpec)
+
+		# 종료 조건 옵션 설정 // Set Stop Condition Options
+		denoisingDiffusionInpaintingDL.SetLearningStopCondition('');
+
+		# DenoisingDiffusionInpaintingDL learn function을 진행하는 스레드 생성 # Create the DenoisingDiffusionInpaintingDL Learn function thread
+		def Learn_thread():
+			global eLearnResult, bTerminated
+			eLearnResult = denoisingDiffusionInpaintingDL.Learn()
+			bTerminated = True
+		
+		def Input_thread():
+			global bEscape
+			while True:
+				if msvcrt.kbhit() and msvcrt.getch() == b'\x1b':  # ESC key
+					bEscape = True
+					break
+		
+		threading.Thread(target=Learn_thread).start()
+		threading.Thread(target=Input_thread, daemon=True).start()
+
+		while not denoisingDiffusionInpaintingDL.IsRunning() and not bTerminated:
+			time.sleep(0.001)
+
+		i32MaxEpoch = denoisingDiffusionInpaintingDL.GetLearningEpoch()
+		i32PrevEpoch = 0
+		i32PrevCostCount = 0
+		i32PrevValidationCount = 0
+
+		while True:
+			time.sleep(0.001)
+
+			# 마지막 미니 배치 최대 반복 횟수 받기 # Get the last maximum number of iterations of the last mini batch 
+			i32MaxIteration = denoisingDiffusionInpaintingDL.GetActualMiniBatchCount()
+			# 마지막 미니 배치 반복 횟수 받기 # Get the last number of mini batch iterations
+			i32Iteration = denoisingDiffusionInpaintingDL.GetLearningResultCurrentIteration()
+			# 마지막 학습 횟수 받기 # Get the last epoch learning
+			i32Epoch = denoisingDiffusionInpaintingDL.GetLastEpoch()
+
+			if i32Epoch != i32PrevEpoch and i32Iteration == i32MaxIteration and i32Epoch > 0:
+				# 학습 결과 비용과 검증 결과 기록을 받아 그래프 뷰에 출력  
+				# Get the history of cost and validation and print it at graph view
+				listCosts = List[Single]()
+				listSSIM = List[Single]()
+				listMRQ = List[Single]()
+				listValidationEpoch = List[Int32]()
+
+				res = denoisingDiffusionInpaintingDL.GetLearningResultAllHistory(listCosts, listSSIM, listMRQ, listValidationEpoch)[0]
+				
+				if listCosts.Count != 0:
+					# 마지막 학습 결과 비용 받기 # Get the last cost of the learning result
+					f32CurrCost = listCosts[listCosts.Count - 1]
+					# 마지막 SSIM 결과 받기 # Get the last SSIM result
+					f32SSIM = listSSIM[listSSIM.Count - 1] if listSSIM.Count != 0 else 0
+					# 마지막 MRQ 결과 받기 # Get the last MRQ result
+					f32MRQ = listMRQ[listMRQ.Count - 1] if listMRQ.Count != 0 else 0
+
+					# 해당 epoch의 비용과 검증 결과 값 출력 # Print cost and validation value for the relevant epoch
+					print("Cost : {:.6f} SSIM : {:.6f} MRQ : {:.6f} Epoch {} / {}".format(f32CurrCost, f32SSIM, f32MRQ, i32Epoch, i32MaxEpoch))
+
+					# 비용 기록이나 검증 결과 기록이 있다면 출력 # Print results if cost or validation history exists
+					if (listCosts.Count != 0 and i32PrevCostCount != listCosts.Count) or (listSSIM.Count != 0 and i32PrevValidationCount != listSSIM.Count):
+						viewGraph.LockUpdate()
+
+						# 이전 그래프의 데이터를 삭제 # Clear previous grpah data
+						viewGraph.Clear()
+						# Graph View 데이터 입력 # Input Graph View Data
+						viewGraph.Plot(listCosts, EChartType.Line, EColor.RED, "Cost")
+
+						i32Step = denoisingDiffusionInpaintingDL.GetLearningValidationStep()
+						listV1 = List[Single]()
+
+						for i in range(listSSIM.Count - 1):
+							listV1.Add((float)(i * i32Step))
+
+						listV1.Add((float)(listCosts.Count - 1))
+						# Graph View 데이터 입력 # Input Graph View Data
+						viewGraph.Plot(listV1, listSSIM, EChartType.Line, EColor.BLUE, "SSIM")
+
+						listV2 = List[Single]()
+
+						for i in range(listMRQ.Count - 1):
+							listV2.Add((float)(i * i32Step))
+
+						listV2.Add((float)(listCosts.Count - 1))
+						# Graph View 데이터 입력 # Input Graph View Data
+						viewGraph.Plot(listV2, listMRQ, EChartType.Line, EColor.GREEN, "MRQ")
+
+						viewGraph.UnlockUpdate()
+						viewGraph.Invalidate()
+
+					if bEscape:
+						denoisingDiffusionInpaintingDL.Stop()
+
+					i32PrevEpoch = i32Epoch
+					i32PrevCostCount = listCosts.Count
+					i32PrevValidationCount = listSSIM.Count
+
+			if denoisingDiffusionInpaintingDL.IsRunning() == False:
+				break
+			
+		if eLearnResult.IsFail():
+			ErrorPrint(eLearnResult, 'Failed to learn.')
+			break
+
+		# Source 이미지 설정 # Set the Source image
+		denoisingDiffusionInpaintingDL.SetInferenceImage(fliSourceImage)
+
+		# 생성할 이미지 설정 # Set the image to create
+		denoisingDiffusionInpaintingDL.SetInferenceResultImage(fliResultImage)
+
+		# 앞서 설정된 파라미터 대로 알고리즘 수행 # Execute algorithm according to previously set parameters
+		if (res := denoisingDiffusionInpaintingDL.Execute()).IsFail():
+			ErrorPrint(res, 'Failed to execute.')
+			break
+
+		viewImageResult.ZoomFit()
+
+		# 이미지 뷰를 갱신 # Update image view
+		viewImageLearn.Invalidate(True)
+		viewImageValidate.Invalidate(True)
+		viewImageSource.Invalidate(True)
+		viewImageResult.Invalidate(True)
+
+		# 그래프 뷰를 갱신 # Update the Graph view.
+		viewGraph.Invalidate(True)
+
+		# 이미지 뷰가 종료될 때 까지 기다림 # Wait for the image view to close
+		while viewImageLearn.IsAvailable() and viewImageSource.IsAvailable() and viewImageValidate.IsAvailable() and viewImageResult.IsAvailable() and viewGraph.IsAvailable():
+			CThreadUtilities.Sleep(1)
+
+		break
+	
+	# End of main function
+
+# 에러 출력 함수 # Error printing function
+def ErrorPrint(res, str):
+	if len(str) > 1:
+		print(str)
+
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
+
+if __name__ == '__main__':
+    main()
+
+
+
