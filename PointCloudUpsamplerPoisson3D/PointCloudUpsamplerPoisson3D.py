68aedd41e707f4eecbe9a86033889260a1bb07b0
mjjeong <mjjeong@fourthlogic.co.kr>
2025-11-14 16:57:29 +0900

    Update 3D Upsamplers' Camera Position

--- PointCloudUpsamplerPoisson3D/PointCloudUpsamplerPoisson3D.py
+++ PointCloudUpsamplerPoisson3D/PointCloudUpsamplerPoisson3D.py
@@ -77,22 +77,13 @@ def main():
 		viewObjectSrc.UpdateAll()
 		viewObjectDst.UpdateAll()
 
-		view3DSrc.SynchronizePointOfView(view3DDst)
-		view3DSrc.SynchronizeWindow(view3DDst)
-		
-		#출력 뷰의 시점을 계산 # Calculate the viewpoint of destination view
-		cam = CFL3DCamera()
-
-		cam.SetProjectionType(E3DCameraProjectionType.Perspective)
-		cam.SetDirection(CFLPoint3[Single](0.327705, 0.066764, -0.942418))
-		cam.SetDirectionUp(CFLPoint3[Single](0.311277, 0.839746, -0.444896))
-		cam.SetPosition(CFLPoint3[Single](-1.825832, 0.425620, 1.548716))
-		cam.SetAngleOfViewY(45)
-
-		view3DDst.SetCamera(cam)
-
 		view3DSrc.UpdateObject(0)
 		view3DDst.UpdateObject(0)
+
+		view3DSrc.SynchronizePointOfView(view3DDst)
+		view3DSrc.SynchronizeWindow(view3DDst)
+
+		view3DSrc.ZoomFit()
 
 		while view3DSrc.IsAvailable() and view3DDst.IsAvailable():
 			CThreadUtilities.Sleep(1)
