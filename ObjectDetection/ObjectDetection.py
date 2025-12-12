735739251ed76631d7b5b488971bc22532a3c7d6
thkim <kth@fourthlogic.co.kr>
2025-12-12 16:28:10 +0900

    Modify Instance name in Object Detection

--- ObjectDetection/ObjectDetection.py
+++ ObjectDetection/ObjectDetection.py
@@ -245,9 +245,9 @@ def main():
 				# 마지막 검증 결과 받기 # Get the last validation result
 				f32Validation = objectDetectionDL.GetLearningResultLastMeanAP()
 				# 마지막 Recall 결과 받기 # Get the last recall result
-				f32Recall = instanceSegmentationDL.GetLearningResultLastRecall();
+				f32Recall = objectDetectionDL.GetLearningResultLastRecall();
 				# 마지막 검증 결과 받기 # Get the last validation result
-				f32Precision = instanceSegmentationDL.GetLearningResultLastPrecision();
+				f32Precision = objectDetectionDL.GetLearningResultLastPrecision();
 				# 해당 epoch의 비용과 검증 결과 값 출력 # Prcost and validation value for the relevant epoch
 				if f32AvgCost < objectDetectionDL.GetLearningRequiredCostForValidation() :
 					print("Cost : {:6f} Avg Cost : {:6f} mAP : {:6f} Recall : {:6f} Precision : {:6f} Epoch {} / {}".format(f32CurrCost, f32AvgCost, f32Validation, f32Recall, f32Precision, i32Epoch, i32MaxEpoch))
@@ -263,7 +263,7 @@ def main():
 				listPrecisionHistory = List[Single]()
 				vctValidationEpoch = List[Int32]()
 
-				objectDetectionDL.GetLearningResultAllHistory(listCostHistory, listAvgCostHistory, listRecallHistory , listPrecisionHistory, listMeanAPHistory, vctValidationEpoch)
+				objectDetectionDL.GetLearningResultAllHistory(listCostHistory, listAvgCostHistory, listMeanAPHistory, vctValidationEpoch, listRecallHistory , listPrecisionHistory)
 
 				# 비용 기록이나 검증 결과 기록이 있다면 출력 # Prresults if cost or validation history exists
 				if((listCostHistory.Count != 0 and i32PrevCostCount != listCostHistory.Count) or (listMeanAPHistory.Count != 0 and i32PrevValidationCount != listMeanAPHistory.Count)):
