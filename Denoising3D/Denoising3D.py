915e92633919f42d67958a889066804d16d63d14
hskim <hskim@fourthlogic.co.kr>
2025-12-17 16:10:14 +0900

    Add Denoising 3D Example

---
+++ Denoising3D/Denoising3D.py
@@ -0,0 +1,182 @@
+﻿# FLImagingClrPy 선언 # Declare FLImagingClrPy
+from FLImagingClrPy import *
+
+# You must call the following function once
+# before using any features of the FLImaging(R) library
+CLibraryUtilities.Initialize()
+
+
+
+# 메인 함수 # Main function
+def main():
+
+	# 3D 객체 선언 # Declare 3D object	
+	floSource = CFL3DObject()
+	floNoiseResult = CFL3DObject()
+	floSmoothingResult = CFL3DObject()
+	floDenoisingResult = CFL3DObject()
+
+	# 3D 뷰 선언 # Declare 3D view	
+	
+	view3DNoiseResult = CGUIView3D()
+	view3DSmoothingResult = CGUIView3D()
+	view3DDenoisingResult = CGUIView3D()
+
+	# 알고리즘 동작 결과 # Algorithm execution result
+	res = CResult()
+
+	while True:		
+		# Source Object 로드 # Load the Source object
+		if(res := floSource.Load("../../ExampleImages/Denoising3D/Cube.ply")).IsFail() :		
+			ErrorPrint(res, "Failed to load the object file.\n")
+			break
+		
+
+		# Noise 결과 3D 뷰 생성 # Create Noise Result 3D View
+		if(res := view3DNoiseResult.Create(100, 0, 612, 512)).IsFail() :		
+			ErrorPrint(res, "Failed to create the Source 3D view.\n")
+			break
+		
+
+		# Smoothing 결과 3D 뷰 생성 # Create Smoothing Result 3D View
+		if(res := view3DSmoothingResult.Create(612, 0, 1124, 512)).IsFail() :		
+			ErrorPrint(res, "Failed to create the Smoothing Result 3D view.\n")
+			break
+		
+
+		# Denoising 결과 3D 뷰 생성 # Create Denoising Result 3D View
+		if(res := view3DDenoisingResult.Create(1124, 0, 1636, 512)).IsFail() :		
+			ErrorPrint(res, "Failed to create the Denoising Result 3D view.\n")
+			break
+		
+		# 노이즈 적용된 객체 생성 # Noise-applied object generation		
+		scannedPointCloudGenerator3D = CScannedPointCloudGenerator3D()
+		scannedPointCloudGenerator3D.SetSourceObject(floSource)
+		scannedPointCloudGenerator3D.SetDestinationObject(floNoiseResult)
+		scannedPointCloudGenerator3D.SetNoiseRatio(0.5)
+		scannedPointCloudGenerator3D.EnableViewPointSetting(False)
+		scannedPointCloudGenerator3D.EnableHiddenPointRemoval(False)
+		scannedPointCloudGenerator3D.EnableIncludingNormalVector(False)
+		scannedPointCloudGenerator3D.EnableRelativeSamplingDistance(False)
+		scannedPointCloudGenerator3D.SetSamplingDistance(1)
+		scannedPointCloudGenerator3D.SetTargetVertexColor(TPoint3[Byte](0, 255, 255))
+
+		if(res := scannedPointCloudGenerator3D.Execute()).IsFail() :		
+			ErrorPrint(res, "Failed to execute the Scanned Point Cloud Generator 3D.\n")
+			break
+		
+
+		# Smoothing3D 객체 생성 # Create Smoothing3D object
+		smoothing3D = CSmoothing3D()
+
+		# Source object 설정 # Set the source object
+		smoothing3D.SetSourceObject(floNoiseResult)
+
+		# Destination object 설정 # Set the destination object
+		smoothing3D.SetDestinationObject(floSmoothingResult)
+
+		# 탐색할 최근접 이웃 수 설정 # Set the number of nearest neighbors to search
+		smoothing3D.SetNeighborCount(100)
+
+		tp3ViewPoint = TPoint3[Single]()
+		tp3ViewPoint.x = 0
+		tp3ViewPoint.y = 0
+		tp3ViewPoint.z = 0
+
+		# 뷰 포인트 설정 # Set the view point
+		smoothing3D.SetViewPoint(tp3ViewPoint)
+
+		# 앞서 설정된 파라미터 대로 알고리즘 수행 # Execute algorithm according to previously set parameters
+		if(res := smoothing3D.Execute()).IsFail() :				
+			ErrorPrint(res, "Failed to execute Smoothing 3D.")
+			break		
+
+		# Denoising3D 객체 생성 # Create Denoising3D object
+		denoising3D = CDenoising3D()
+
+		# Source object 설정 # Set the source object
+		denoising3D.SetSourceObject(floNoiseResult)
+
+		# Destination object 설정 # Set the destination object
+		denoising3D.SetDestinationObject(floDenoisingResult)
+
+		# lambda 값 설정 # Set the lambda value
+		denoising3D.SetLambda(0.1)
+
+		# 탐색할 최근접 이웃 수 설정 # Set the number of nearest neighbors to search
+		denoising3D.SetNeighborCount(100)
+
+		# 뷰 포인트 설정 # Set the view point
+		denoising3D.SetViewPoint(tp3ViewPoint)
+
+		# 앞서 설정된 파라미터 대로 알고리즘 수행 # Execute algorithm according to previously set parameters
+		if(res := Denoising3D.Execute()).IsFail() :	
+			ErrorPrint(res, "Failed to execute Denoising 3D.")
+			break
+		
+
+		# 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 # Obtain layer 0 number from image view for display
+		# 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 # This object belongs to an image view and does not need to be released separately		
+		layer3DNoiseResult = view3DNoiseResult.GetLayer(0)
+		layer3DDenoisingResult = view3DDenoisingResult.GetLayer(0)
+		layer3DSmoothingResult = view3DSmoothingResult.GetLayer(0)
+		
+		# 기존에 Layer에 그려진 도형들을 삭제 # Clear the figures drawn on the existing layer
+		layer3DNoiseResult.Clear()
+		layer3DDenoisingResult.Clear()
+		layer3DSmoothingResult.Clear()
+
+		# View 정보를 디스플레이 합니다. # Display View information.
+		# 아래 함수 DrawTextCanvas은 Screen좌표를 기준으로 하는 String을 Drawing 한다.# The function DrawTextCanvas below draws a String based on the screen coordinates.
+		# 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+		#                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+		# Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+		#                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+		flp = CFLPoint[Double]()
+
+		if(res := layer3DNoiseResult.DrawTextCanvas(flp, "Noise(0.5)", EColor.YELLOW, EColor.BLACK, 20)).IsFail() :		
+			ErrorPrint(res, "Failed to draw text.\n")
+			break
+		
+		if(res := layer3DDenoisingResult.DrawTextCanvas(flp, "Denoising Result", EColor.YELLOW, EColor.BLACK, 20)).IsFail() :		
+			ErrorPrint(res, "Failed to draw text.\n")
+			break
+
+		if(res := layer3DSmoothingResult.DrawTextCanvas(flp, "Smoothing Result", EColor.YELLOW, EColor.BLACK, 20)).IsFail() :		
+			ErrorPrint(res, "Failed to draw text.\n")
+			break
+		
+		# 3D 오브젝트 뷰에 결과 오브젝트 디스플레이
+		if (res := view3DNoiseResult.PushObject(floNoiseResult)).IsFail() or \
+			(res := layer3DDenoisingResult.PushObject(floDenoisingResult)).IsFail() or \
+			(res := layer3DSmoothingResult.PushObject(floSmoothingResult)).IsFail() :
+			ErrorPrint(res, "Failed to set object on the 3D View.\n")
+			break
+
+		view3DNoiseResult.SynchronizePointOfView(view3DDenoisingResult)
+		view3DNoiseResult.SynchronizePointOfView(view3DSmoothingResult)
+		view3DNoiseResult.ZoomFit()
+            	
+		# 이미지 뷰를 갱신 합니다. # Update image view
+		view3DNoiseResult.Invalidate(True)
+		view3DDenoisingResult.Invalidate(True)
+		view3DSmoothingResult.Invalidate(True)
+
+		#이미지 뷰, 3D 뷰가 종료될 때 까지 기다림 # Wait for the image and 3D view to close
+		while view3DNoiseResult.IsAvailable() and view3DDenoisingResult.IsAvailable() and view3DSmoothingResult.IsAvailable() :
+			CThreadUtilities.Sleep(1)
+
+		break
+	
+	# End of main function
+
+# 에러 출력 함수 # Error printing function
+def ErrorPrint(res, str):
+	if len(str) > 1:
+		print(str)
+
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}\n')
+
+
+if __name__ == '__main__':
+    main()
