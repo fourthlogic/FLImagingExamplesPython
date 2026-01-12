4be85ac951e9e9aa18060139e4eca15b75886574
khs <khs@fourthlogic.co.kr>
2026-01-12 20:29:22 +0900

    Update Inpainting Coherence Transport, Fast Marching Method, Navier Stokes Example (Image Figures as Inpainting Regions API Update)

--- InpaintingCoherenceTransport/InpaintingCoherenceTransport.py
+++ InpaintingCoherenceTransport/InpaintingCoherenceTransport.py
@@ -89,6 +89,9 @@ def main():
 		mvChannelWeight = CMultiVar[Double](0.114, 0.587, 0.299)
 		inpaintingCoherenceTransport.SetWeightOfChannels(mvChannelWeight)
 
+		# Image figure 를 inpainting region으로 설정하지 않음 # Do not set image figure as inpainting region
+		inpaintingCoherenceTransport.EnableImageFiguresAsInpaintingRegions(False);
+
 		# 미리 그려둔 Painting region Figure Array 불러오기 # Load Pre-drawn Painting Region Figure Array
 		flrInpaintingRegion = CFLRegion()
 
--- InpaintingNavierStokes/InpaintingNavierStokes.py
+++ InpaintingNavierStokes/InpaintingNavierStokes.py
@@ -76,6 +76,9 @@ def main():
 		# Inpainting에 사용될 픽셀 영역 설정 # Setting the pixel area to be used for Inpainting
 		inpaintingNavierStokes.SetEpsilon(5)
 		
+		# Image figure 를 inpainting region으로 설정하지 않음 # Do not set image figure as inpainting region
+		inpaintingNavierStokes.EnableImageFiguresAsInpaintingRegions(False);
+
 		# 미리 그려둔 Painting region Figure Array 불러오기 # Load Pre-drawn Painting Region Figure Array
 		flfaInpaintingRegion = CFLFigureArray()
 
