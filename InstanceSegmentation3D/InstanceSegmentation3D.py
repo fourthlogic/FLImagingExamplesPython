eb81e30850b984a5475561b3d4110979ce6123fd
sbno <sbno@fourthlogic.co.kr>
2026-01-13 17:42:27 +0900

    Add Instance Segmentation 3D example

---
+++ InstanceSegmentation3D/InstanceSegmentation3D.py
@@ -0,0 +1,403 @@
+﻿# FLImagingClrPy 선언 # Declare FLImagingClrPy
+from tokenize import Single, String
+from FLImagingClrPy import *
+
+# You must call the following function once
+# before using any features of the FLImaging(R) library
+CLibraryUtilities.Initialize()
+
+import threading
+import time
+import msvcrt
+
+bEscape = False
+bTerminated = False
+eLearnResult = CResult()
+
+# 메인 함수 # Main function
+def main():
+
+	# 이미지 객체 선언 # Declare the image object
+	fliLearnImage = CFLImage()
+	fliDepthMapImage = CFLImage()
+	fliResultBoxContourImage = CFLImage()
+	flogResult = CFL3DObjectGroup()
+
+	# 이미지 뷰 선언 # Declare the image view
+	viewImageLearn = CGUIViewImage()
+	viewImageDepthMap = CGUIViewImage()
+	viewImagesBoxContour = CGUIViewImage()
+	view3DResult = CGUIView3D()
+	
+	# 그래프 뷰 선언 # Declare the graph view
+	viewGraph = CGUIViewGraph()
+	bTerminated = False
+
+	while True:
+		# 이미지 로드 # Load image
+		if (res := fliLearnImage.Load('../../ExampleImages/InstanceSegmentation3D/Texture(Labeled).flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		if (res := fliDepthMapImage.Load('../../ExampleImages/InstanceSegmentation3D/Depth.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image file.')
+			break
+
+		# learn 이미지 뷰 생성 # Create learn image view
+		if (res := viewImageLearn.Create(100, 0, 600, 500)).IsFail():
+			ErrorPrint(res, 'Failed to create the image view.')
+			break
+		
+		if((res := viewImageDepthMap.Create(600, 0, 1100, 500)).IsFail()):
+			ErrorPrint(res, "Failed to create the image view.")
+			break
+
+		if((res := viewImagesBoxContour.Create(100, 500, 600, 1000)).IsFail()):
+			ErrorPrint(res, "Failed to create the image view.")
+			break
+
+		if((res := view3DResult.Create(600, 500, 1100, 1000)).IsFail()):
+			ErrorPrint(res, "Failed to create the image view.")
+			break
+
+		# Graph 뷰 생성 # Create graph view
+		if((res := viewGraph.Create(1100, 0, 1600, 500)).IsFail()):
+			ErrorPrint(res, "Failed to create the image view.")
+			break
+
+		viewGraph.SetDarkMode()
+
+		# 이미지 뷰에 이미지를 디스플레이 # display the image in the imageview
+		# 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [0], ... [n-1] 형태로 tuple 을 반환한다. # A function that receives parameters returns a tuple structured as [return], [0], ... [n-1].
+		if (res := viewImageLearn.SetImagePtr(fliLearnImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+		
+		if (res := viewImageDepthMap.SetImagePtr(fliDepthMapImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		if (res := viewImagesBoxContour.SetImagePtr(fliResultBoxContourImage)[0]).IsFail():
+			ErrorPrint(res, 'Failed to set image object on the image view.')
+			break
+
+		# 두 이미지 뷰의 시점을 동기화 한다 # Synchronize the viewpoints of the two image views
+		# 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [0], ... [n-1] 형태로 tuple 을 반환한다. # A function that receives parameters returns a tuple structured as [return], [0], ... [n-1].
+		if (res := viewImageLearn.SynchronizePointOfView(viewImageDepthMap)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize view.')
+			break
+
+		# 두 이미지 뷰의 시점을 동기화 한다 # Synchronize the viewpoints of the two image views
+		# 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [0], ... [n-1] 형태로 tuple 을 반환한다. # A function that receives parameters returns a tuple structured as [return], [0], ... [n-1].
+		if (res := viewImageLearn.SynchronizePointOfView(viewImagesBoxContour)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize view.')
+			break
+
+		# 두 이미지 뷰 윈도우의 위치를 맞춤 # Synchronize the positions of the two image view windows
+		# 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [0], ... [n-1] 형태로 tuple 을 반환한다. # A function that receives parameters returns a tuple structured as [return], [0], ... [n-1].
+		if (res := viewImageLearn.SynchronizeWindow(viewImageDepthMap)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize window.')
+			break
+		
+		# 두 이미지 뷰 윈도우의 위치를 맞춤 # Synchronize the positions of the two image view windows
+		# 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [0], ... [n-1] 형태로 tuple 을 반환한다. # A function that receives parameters returns a tuple structured as [return], [0], ... [n-1].
+		if (res := viewImageLearn.SynchronizeWindow(viewImagesBoxContour)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize window.')
+			break
+
+		# 두 이미지 뷰 윈도우의 위치를 맞춤 # Synchronize the positions of the two image view windows
+		# 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [0], ... [n-1] 형태로 tuple 을 반환한다. # A function that receives parameters returns a tuple structured as [return], [0], ... [n-1].
+		if (res := viewImageLearn.SynchronizeWindow(view3DResult)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize window.')
+			break
+
+		# 두 이미지 뷰 윈도우의 위치를 맞춤 # Synchronize the positions of the two image view windows
+		# 파라미터를 입력 받는 함수는 리턴이 tuple로 생성되며 [return], [0], ... [n-1] 형태로 tuple 을 반환한다. # A function that receives parameters returns a tuple structured as [return], [0], ... [n-1].
+		if (res := viewImageLearn.SynchronizeWindow(viewGraph)[0]).IsFail():
+			ErrorPrint(res, 'Failed to synchronize window.')
+			break
+
+		# 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 # Obtain layer 0 number from image view for display
+		# 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 # This object belongs to an image view and does not need to be released separately
+		layerLearn = viewImageLearn.GetLayer(0)
+		layerValidation = viewImageDepthMap.GetLayer(0)
+		layerResultLabel = viewImagesBoxContour.GetLayer(0)
+		layer3DResult = view3DResult.GetLayer(0)
+	
+		# 기존에 Layer에 그려진 도형들을 삭제 # Clear the figures drawn on the existing layer
+		layerLearn.Clear()
+		layerValidation.Clear()
+		layerResultLabel.Clear()
+		layer3DResult.Clear()
+		
+		# View 정보를 디스플레이 합니다. # Display View information.
+		# 아래 함수 DrawTextCanvas은 Screen좌표를 기준으로 하는 String을 Drawing 한다.# The function DrawTextCanvas below draws a String based on the screen coordinates.
+		# 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+		#                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+		# Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+		#                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+		flpPoint = CFLPoint[Double](0, 0)
+
+		if((res := layerLearn.DrawTextCanvas(flpPoint, "LEARN & VALIDATION & INFERENCE", EColor.YELLOW, EColor.BLACK, 30)).IsFail()):
+			ErrorPrint(res, "Failed to draw text")
+			break
+		
+		if((res := layerValidation.DrawTextCanvas(flpPoint, "LEARN & INFERENCE DEPTH MAP", EColor.YELLOW, EColor.BLACK, 30)).IsFail()):
+			ErrorPrint(res, "Failed to draw text")
+			break
+
+		if((res := layerResultLabel.DrawTextCanvas(flpPoint, "RESULT BOX CONTOUR", EColor.YELLOW, EColor.BLACK, 30)).IsFail()):
+			ErrorPrint(res, "Failed to draw text")
+			break
+
+		if((res := layer3DResult.DrawTextCanvas(flpPoint, "Match 3D RESULT", EColor.YELLOW, EColor.BLACK, 30)).IsFail()):
+			ErrorPrint(res, "Failed to draw text")
+			break
+
+		# 이미지 뷰를 갱신 # Update the image view.
+		viewImageLearn.Invalidate(True)
+		viewImageDepthMap.Invalidate(True)
+		viewImagesBoxContour.Invalidate(True)
+		view3DResult.Invalidate(True)
+		
+		# InstanceSegmentation3D 객체 생성 # Create InstanceSegmentation3D object
+		instanceSegmentation3DDL = CInstanceSegmentation3DDL()
+
+		# OptimizerSpec 객체 생성 # Create OptimizerSpec object
+		optSpec = COptimizerSpecAdamGradientDescent()
+
+		# 학습할 이미지 설정 # Set the image to learn
+		instanceSegmentation3DDL.SetLearningImage(fliLearnImage)
+		# 검증할 이미지 설정 # Set the image to validation
+		instanceSegmentation3DDL.SetLearningValidationImage(fliLearnImage)
+		# 학습할 Depth Map 설정 # Set the depth map to learn
+		instanceSegmentation3DDL.SetLearningDepthMap(fliDepthMapImage);
+
+		# 학습할 InstanceSegmentation3D 모델 설정 # Set up the InstanceSegmentation3D model to learn
+		instanceSegmentation3DDL.SetModel(CInstanceSegmentation3DDL.EModel.R_FLSegNet)
+		# 학습할 InstanceSegmentation3D 모델 Version 설정 # Set up the InstanceSegmentation3D model version to learn
+		instanceSegmentation3DDL.SetModelVersion(CInstanceSegmentation3DDL.EModelVersion.R_FLSegNet_V1_256)
+		# 학습 epoch 값을 설정 # Set the learn epoch value 
+		instanceSegmentation3DDL.SetLearningEpoch(1000)
+		# 학습 이미지 Interpolation 방식 설정 # Set Interpolation method of learn image
+		instanceSegmentation3DDL.SetInterpolationMethod(EInterpolationMethod.Bilinear)
+		# 학습을 종료할 조건식 설정. mAP, Precision값이 1 이상인 경우 학습 종료한다.mAP 와 metric는 동일한 값입니다.
+		# Set Conditional Expression to End Learning. If the mAP, Precision value is 1 or higher, end the learning. mAP is same value as metric.
+		instanceSegmentation3DDL.SetLearningStopCondition("epoch >= 300 & mAP >= 1 & Precision >= 1")
+
+		flpFocalLength = CFLPoint[Single](1000.0, 1000.0)
+		flpPrincipalPoint = CFLPoint[Single](0.0, 0.0)
+		mvDistortionCoefficient = CMultiVar[Double]()
+
+		mvDistortionCoefficient.PushBack(0.0);
+		mvDistortionCoefficient.PushBack(0.0);
+		mvDistortionCoefficient.PushBack(0.0);
+		mvDistortionCoefficient.PushBack(0.0);
+		mvDistortionCoefficient.PushBack(0.0);
+
+		# 학습 시의 Camera Calibrator 설정 # Set the camera calibrator to learn
+		instanceSegmentation3DDL.SetLearningCameraCalibrator(flpFocalLength, flpPrincipalPoint, 1.0, mvDistortionCoefficient, EDirectionType.Decrement);
+
+		# Optimizer의 학습률 설정 # Set learning rate of Optimizer
+		optSpec.SetLearningRate(0.0001)
+
+		# 설정한 Optimizer를 InstanceSegmentation3D에 적용 # Apply Optimizer that we set up to InstanceSegmentation3D
+		instanceSegmentation3DDL.SetLearningOptimizerSpec(optSpec)
+		
+		# AugmentationSpec 설정 # Set the AugmentationSpec
+		augSpec = CAugmentationSpec()
+
+		augSpec.EnableAugmentation(True)
+		augSpec.SetCommonIoUThreshold(0.800000)
+		augSpec.SetCommonInterpolationMethod(EInterpolationMethod.Bilinear)
+		augSpec.EnableHorizontalFlip(True)
+		augSpec.EnableVerticalFlip(True)
+
+		instanceSegmentation3DDL.SetLearningAugmentationSpec(augSpec)
+
+		# 자동 저장 옵션 설정 # Set Auto-Save Options
+		autoSaveSpec = CAutoSaveSpec()
+
+		# 자동 저장 활성화 # Enable Auto-Save
+		# 저장 때문에 발생하는 속도 저하를 막기 위해 예제에서는 코드 사용법만 표시하고 옵션은 끔 # To prevent performance degradation caused by saving, the examples only demonstrate how to use the code, with the saving option disabled.
+		autoSaveSpec.EnableAutoSave(False)
+		# 저장할 모델 경로 설정 # Set Model path to save
+		autoSaveSpec.SetAutoSavePath("model.flis3")
+		# 자동 저장 조건식 설정. 현재 cost값이 최소이고 accuracy값이 최대 값인 경우 저장 활성화
+		# Set auto-save conditional expressions. Enable save if the current cost value is minimum and the accumulation value is maximum
+		autoSaveSpec.SetAutoSaveCondition("map > max('map')")
+
+		# 자동 저장 옵션 설정 # Set Auto-Save Options
+		instanceSegmentation3DDL.SetLearningAutoSaveSpec(autoSaveSpec)
+
+		# InstanceSegmentation3D learn function을 진행하는 스레드 생성 # Create the InstanceSegmentation3D Learn function thread
+		def Learn_thread():
+			global eLearnResult, bTerminated
+			eLearnResult = instanceSegmentation3DDL.Learn()
+			bTerminated = True
+		
+		def Input_thread():
+			global bEscape
+			while True:
+				if msvcrt.kbhit() and msvcrt.getch() == b'\x1b':  # ESC key
+					bEscape = True
+					break
+		
+		threading.Thread(target=Learn_thread).start()
+		threading.Thread(target=Input_thread, daemon=True).start()
+
+		while not instanceSegmentation3DDL.IsRunning() and not bTerminated:
+			time.sleep(0.001)
+
+		i32MaxEpoch = instanceSegmentation3DDL.GetLearningEpoch()
+		i32PrevEpoch = 0
+		i32PrevCostCount = 0
+		i32PrevValidationCount = 0
+
+		while(True):
+			time.sleep(0.001)
+
+			# 마지막 미니 배치 반복 횟수 받기 # Get the last maximum number of iterations of the last mini batch 
+			i32MiniBatchCount = instanceSegmentation3DDL.GetActualMiniBatchCount()
+			# 마지막 미니 배치 반복 횟수 받기 # Get the last number of mini batch iterations
+			i32Iteration = instanceSegmentation3DDL.GetLearningResultCurrentIteration()
+			# 마지막 학습 횟수 받기 # Get the last epoch learning
+			i32Epoch = instanceSegmentation3DDL.GetLastEpoch()
+			
+			# 미니 배치 반복이 완료되면 cost와 validation 값을 디스플레이 
+			# Display cost and validation value if iterations of the mini batch is completed 
+			if i32Epoch != i32PrevEpoch and i32Iteration == i32MiniBatchCount and i32Epoch > 0:
+				# 마지막 학습 결과 비용 받기 # Get the last cost of the learning result
+				f32CurrCost = instanceSegmentation3DDL.GetLearningResultLastCost()
+				# 마지막 검증 결과 받기 # Get the last validation result
+				f32MeanAP = instanceSegmentation3DDL.GetLearningResultLastMeanAP()
+				# 마지막 Recall 결과 받기 # Get the last recall result
+				f32Recall = instanceSegmentation3DDL.GetLearningResultLastRecall();
+				# 마지막 검증 결과 받기 # Get the last validation result
+				f32Precision = instanceSegmentation3DDL.GetLearningResultLastPrecision();
+
+				# 해당 epoch의 비용과 검증 결과 값 출력 # Prcost and validation value for the relevant epoch
+				print("Cost : {:6f} mAP : {:6f} Recall : {:6f} Precision : {:6f} Epoch {} / {}".format(f32CurrCost, f32MeanAP, f32Recall, f32Precision, i32Epoch, i32MaxEpoch))
+
+				# 학습 결과 비용과 검증 결과 기록을 받아 그래프 뷰에 출력  
+				# Get the history of cost and validation and prit at graph view
+				listCostHistory = List[Single]()
+				listMeanAPHistory = List[Single]()
+				listRecall = List[Single]()
+				listPrecision = List[Single]()
+				listValidationEpoch = List[Int32]()
+
+				instanceSegmentation3DDL.GetLearningResultAllHistory(listCostHistory, listMeanAPHistory, listValidationEpoch, listRecall, listPrecision)
+
+				# 비용 기록이나 검증 결과 기록이 있다면 출력 # Prresults if cost or validation history exists
+				if((listCostHistory.Count != 0 and i32PrevCostCount != listCostHistory.Count) or (listMeanAPHistory.Count != 0 and i32PrevValidationCount != listMeanAPHistory.Count)):
+					i32Step = instanceSegmentation3DDL.GetLearningValidationStep()
+					listX = List[Single]()
+
+					for i in range(listMeanAPHistory.Count - 1):
+						listX.Add((i * i32Step))
+
+					listX.Add((listCostHistory.Count - 1))
+
+					# 이전 그래프의 데이터를 삭제 # Clear previous grpah data
+					viewGraph.LockUpdate()
+					viewGraph.Clear()
+
+					# Graph View 데이터 입력 # Input Graph View Data
+					viewGraph.Plot(listCostHistory, EChartType.Line, EColor.RED, "Cost")
+					# Graph View 데이터 입력 # Input Graph View Data
+					viewGraph.Plot(listX, listMeanAPHistory, EChartType.Line, EColor.CYAN, "mAP")
+					viewGraph.Plot(listX, listRecall, EChartType.Line, EColor.GREEN, "recall");
+					viewGraph.Plot(listX, listPrecision, EChartType.Line, EColor.PURPLE, "precision");
+					viewGraph.UnlockUpdate()
+
+					viewGraph.UpdateWindow()
+					viewGraph.Invalidate()
+
+				if(bEscape):
+					instanceSegmentation3DDL.Stop()
+
+				i32PrevEpoch = i32Epoch
+				i32PrevCostCount = listCostHistory.Count
+				i32PrevValidationCount = listMeanAPHistory.Count
+
+			# epoch만큼 학습이 완료되면 종료 # End when learning progresses as much as epoch
+			if(instanceSegmentation3DDL.IsRunning() == False):
+				break
+			
+		if eLearnResult.IsFail():
+			ErrorPrint(eLearnResult, 'Failed to execute.')
+			break
+
+		# Result Image에 Box & Contour 모두 출력하는 Execute # Execute to print both Box& Contour in Result Image
+		# 분류할 이미지 설정 # Set the image to classify
+		instanceSegmentation3DDL.SetInferenceImage(fliLearnImage)
+		# 3D Match에 필요한 Depth Map 이미지 설정 # Set the depth map image for 3D Match
+		instanceSegmentation3DDL.SetInferenceDepthMap(fliDepthMapImage);
+		# 추론 결과 이미지 설정 # Set the inference result Image
+		instanceSegmentation3DDL.SetInferenceResultImage(fliResultBoxContourImage)
+		# 추론 결과 옵션 설정 # Set the inference result options
+		# Figure 옵션 설정 # Set the option of figures
+		eFigureOption = Enum.ToObject(CInstanceSegmentation3DDL.EInferenceResultItemSettings, int(CInstanceSegmentation3DDL.EInferenceResultItemSettings.ClassNum) | int(CInstanceSegmentation3DDL.EInferenceResultItemSettings.ClassName) | int(CInstanceSegmentation3DDL.EInferenceResultItemSettings.Objectness) | int(CInstanceSegmentation3DDL.EInferenceResultItemSettings.BoundaryRect) | int(CInstanceSegmentation3DDL.EInferenceResultItemSettings.Contour))
+		instanceSegmentation3DDL.SetInferenceResultItemSettings(eFigureOption)
+
+		# 알고리즘 수행 # Execute the algorithm
+		if((res := instanceSegmentation3DDL.Execute()).IsFail()):
+			ErrorPrint(res, "Failed to execute.")
+			break
+
+		instanceSegmentation3DDL.GetInferenceResultObjectGroup(0, flogResult)
+
+		i64ResultCount = flogResult.GetObjectCount();
+
+		for i in range(i64ResultCount):
+			floResult = flogResult.GetObjectByIndex(i)
+
+			if((res := view3DResult.PushObject(floResult)).IsFail()):
+				ErrorPrint(res, "Failed to display the 3D object.")
+				break
+
+			sResult = SPoseMatrixParametersMulti()
+			tp3Min = TPoint3[Single]()
+			tp3Max = TPoint3[Single]()
+			tp3Mid = TPoint3[Double]()
+
+			floResult.GetBoundingBox(tp3Min, tp3Max)
+			tp3Mid.x = (tp3Min.x + tp3Max.x) * .5
+			tp3Mid.y = (tp3Min.y + tp3Max.y) * .5
+			tp3Mid.z = (tp3Min.z + tp3Max.z) * .5
+			# Pose Matrix 정보를 Layer 에 출력합니다. # Outputs Pose Matrix information to Layer.
+			instanceSegmentation3DDL.GetInferenceResultPoseMatrix(0, i, sResult)
+			layer3DResult.DrawText3D(tp3Mid, "[{0}]\nName: {1}\nR({2}, {3}, {4})\nRVec({5}, {6}, {7})\nT({8}, {9}, {10})\nScore: {11}\nResidual: {12}".format(i, sResult.strClassName, sResult.tp3Angle.x, sResult.tp3Angle.y, sResult.tp3Angle.z, sResult.tp3RotationVector.x, sResult.tp3RotationVector.y, sResult.tp3RotationVector.z, sResult.tp3TranslationVector.x, sResult.tp3TranslationVector.y, sResult.tp3TranslationVector.z, sResult.f64Score, sResult.f64Residual), EColor.YELLOW, EColor.BLACK)
+		
+		# 결과 이미지를 이미지 뷰에 맞게 조정합니다. # Fit the result image to the image view.
+		viewImagesBoxContour.ZoomFit()
+		view3DResult.ZoomFit()
+
+		# 이미지 뷰를 갱신 # Update the image view.
+		viewImageLearn.RedrawWindow()
+		viewImageDepthMap.RedrawWindow()
+		viewImagesBoxContour.RedrawWindow()
+		view3DResult.RedrawWindow()
+			
+		# 그래프 뷰를 갱신 # Update the Graph view.
+		viewGraph.Invalidate(True)
+
+		# # 이미지 뷰가 닫히기 전까지 종료하지 않고 대기 # Wait until the image view is closed before exiting
+		while viewImageLearn.IsAvailable() and viewImageDepthMap.IsAvailable() and viewImagesBoxContour.IsAvailable() and view3DResult.IsAvailable() and viewGraph.IsAvailable():
+			CThreadUtilities.Sleep(1)
+
+		break
+	
+	# End of main function
+
+# 에러 출력 함수 # Error printing function
+def ErrorPrint(res, str):
+	if len(str) > 1:
+		print(str)
+
+	print(f'Error code : {res.GetResultCode()}\nError name : {res.GetString()}')
+
+
+if __name__ == '__main__':
+    main()
