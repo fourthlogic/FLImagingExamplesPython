e1f27cfc79b4748265cadd3b6e49ea42a9204c44
sbno <sbno@fourthlogic.co.kr>
2026-02-02 09:51:44 +0900

    Modify Oriented Object Detection comment

--- InstanceSegmentation3D/InstanceSegmentation3D.py
+++ InstanceSegmentation3D/InstanceSegmentation3D.py
@@ -273,7 +273,7 @@ def main():
 				f32MeanAP = instanceSegmentation3DDL.GetLearningResultLastMeanAP()
 				# 마지막 Recall 결과 받기 # Get the last recall result
 				f32Recall = instanceSegmentation3DDL.GetLearningResultLastRecall();
-				# 마지막 검증 결과 받기 # Get the last validation result
+				# 마지막 Precision 결과 받기 # Get the last precision result
 				f32Precision = instanceSegmentation3DDL.GetLearningResultLastPrecision();
 
 				# 해당 epoch의 비용과 검증 결과 값 출력 # Prcost and validation value for the relevant epoch
