631fc45966fd9c3c2d914b1cbb1b896c75ef4581
shhwang <shhwang@fourthlogic.co.kr>
2026-05-15 09:53:15 +0900

    Modify Speckle Noise Generator Comments

--- NoiseGenerator_Speckle/NoiseGenerator_Speckle.py
+++ NoiseGenerator_Speckle/NoiseGenerator_Speckle.py
@@ -82,7 +82,7 @@ def main():
 		
 		# 생성할 노이즈 타입 설정 # Set the noise type to generate
 		noiseGenerator.SetNoiseType(CNoiseGenerator.ENoiseType.SpeckleNoise)
-		# 간섭 잡음의 분포 감마값 설정 # Set the distribution gamma value of speckle noise
+		# 간섭 잡음의 강도 설정 # Set the intensity of speckle noise
 		noiseGenerator.SetSpeckleIntensity(0.05);
 
 		# 앞서 설정된 파라미터 대로 알고리즘 수행 # Execute algorithm according to previously set parameters
