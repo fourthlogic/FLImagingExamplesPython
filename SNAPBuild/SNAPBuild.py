231b00cd4e1a183f0e6d4f93b1eb1c9c98fe1ee1
ks <ks@fourthlogic.co.kr>
2026-09-07 09:45:21 +0900

    Update parameter Set/Get in the SNAP build example.

--- SNAPBuild/SNAPBuild.py
+++ SNAPBuild/SNAPBuild.py
@@ -11,22 +11,58 @@ CLibraryUtilities.Initialize()
 def main():
 
 	# 스냅 빌드 객체 선언 # Declare SNAP Build
-	buildSNAP = CSNAPBuild()
+	snapBuild = CSNAPBuild()
 
 	while True:
 
 		# 스냅 파일 로드 # Load SNAP file
-		if (res := buildSNAP.Load('../../FLImagingExamplesSNAP/Advanced Functions/Object/Blob/Blob.flsf')).IsFail():
+		if (res := snapBuild.Load('Example.flsf')).IsFail():
 			ErrorPrint(res, 'Failed to load the file.')
 			break
 
+		# 소스 이미지 노드를 찾습니다. # Finds the source image node.
+		nodeSourceImage = snapBuild.FindNode('Image', 'Source Image');
+
+		if nodeSourceImage is None:
+			res = CResult(EResult.FailedToFind);
+			ErrorPrint(res, 'Failed to find the node.');
+			break;
+		
+
+		# 임계값 처리 결과 노드를 찾습니다. # Finds the threshold result node.
+		nodeThresholdResult = snapBuild.FindNode('MultiVar<Double>', 'Threshold Result');
+
+		if nodeThresholdResult is None:
+			res = CResult(EResult.FailedToFind);
+			ErrorPrint(res, 'Failed to find the node.');
+			break;
+
+		fliSource = CFLImage();
+
+		# 소스 이미지를 로드합니다. # Loads the source image.
+		if (res := fliSource.Load('..\\..\\ExampleImages\\Blob\\Ball.flif')).IsFail():
+			ErrorPrint(res, 'Failed to load the image.');
+			break;
+
+		# 노드에 소스 이미지를 설정합니다. # Sets the source image to the node.
+		if (res := nodeSourceImage.SetParameter('Image', fliSource)).IsFail():
+			ErrorPrint(res, 'Failed to set the parameter.');
+			break;
+
 		# 스냅 실행 # Run SNAP
-		if (res := buildSNAP.Run()).IsFail():
+		if (res := snapBuild.Run()).IsFail():
 			ErrorPrint(res, 'Failed to run the SNAP.')
 			break
 
+		# 스냅 실행이 종료 될때까지 대기합니다. # Waits until the SNAP run is complete.
+		snapBuild.WaitStop();
+
+		listThresholdResult = List[Double]();
+
+		res = nodeThresholdResult.GetParameter('Multi Variable', listThresholdResult);
+
 		# 스냅이 종료될 때 까지 기다림 # Wait for the SNAP to close
-		while buildSNAP.IsAvailable():
+		while snapBuild.IsAvailable():
 			CThreadUtilities.Sleep(1)
 
 		break
